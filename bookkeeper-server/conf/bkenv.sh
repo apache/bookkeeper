afdd3dd1e7e06f97e29ff5bcaa2001f81f6d9210
Ivan Brendan Kelly <ivank@apache.org>
2011-08-30 08:34:31 +0000

    BOOKKEEPER-28: Create useful startup scripts for bookkeeper and hedwig (ivank)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/bookkeeper/trunk@1163137 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bookkeeper-server/conf/bkenv.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+#/**
+# * Copyright 2007 The Apache Software Foundation
+# *
+# * Licensed to the Apache Software Foundation (ASF) under one
+# * or more contributor license agreements.  See the NOTICE file
+# * distributed with this work for additional information
+# * regarding copyright ownership.  The ASF licenses this file
+# * to you under the Apache License, Version 2.0 (the
+# * "License"); you may not use this file except in compliance
+# * with the License.  You may obtain a copy of the License at
+# *
+# *     http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# */
+
+# default settings for starting bookkeeper
+
+# Zookeeper ensemble for bookkeeper to use
+#BOOKIE_ZOOKEEPER=
+
+# Port for bookie to listen on
+#BOOKIE_PORT=
+
+# Directory Bookkeeper outputs its write ahead log
+#BOOKIE_TXN_LOGDIR=
+
+# Directory Bookkeeper outputs ledger snapshots
+#BOOKIE_DATA_DIR=
