db71147d9df0ad9efa697275e934330d21c99a88
Sijie Guo <sijie@apache.org>
2012-07-05 05:26:09 +0000

    BOOKKEEPER-296: It's better provide stop script for bookie (nijel via sijie)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/bookkeeper/trunk@1357466 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bookkeeper-server/bin/bookkeeper-daemon.sh
@@ -0,0 +1,146 @@
+#!/usr/bin/env bash
+#
+#/**
+# * Licensed to the Apache Software Foundation (ASF) under one
+# * or more contributor license agreements.  See the NOTICE file
+# * distributed with this work for additional information
+# * regarding copyright ownership.  The ASF licenses this file
+# * to you under the Apache License, Version 2.0 (the
+# * "License"); you may not use this file except in compliance
+# * with the License.  You may obtain a copy of the License at
+# *
+# *     http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+# */
+
+usage="Usage: bookkeeper-daemon.sh (start|stop) <command> <args...>"
+supportedargs="Supported args : -force (accepted only with stop command) - Decides whether to stop the Bookie Server forcefully if not stopped by normal shutdown"
+
+BINDIR=`dirname "$0"`
+BK_HOME=`cd $BINDIR/..;pwd`
+
+if [ -f $BK_HOME/conf/bkenv.sh ]
+then
+ . $BK_HOME/conf/bkenv.sh
+fi
+
+BOOKIE_LOG_DIR=${BOOKIE_LOG_DIR:-"$BK_HOME/logs"}
+
+BOOKIE_ROOT_LOGGER=${BOOKIE_ROOT_LOGGER:-'INFO,ROLLINGFILE'}
+
+BOOKIE_STOP_TIMEOUT=${BOOKIE_STOP_TIMEOUT:-30}
+
+BOOKIE_PID_DIR=${BOOKIE_PID_DIR:-$BK_HOME/bin}
+
+startStop=$1
+shift
+command=$1
+shift
+
+export BOOKIE_LOG_DIR=$BOOKIE_LOG_DIR
+export BOOKIE_ROOT_LOGGER=$BOOKIE_ROOT_LOGGER
+export BOOKIE_LOG_FILE=bookkeeper-$command-$HOSTNAME.log
+
+pid=$BOOKIE_PID_DIR/bookkeeper-$command.pid
+out=$BOOKIE_LOG_DIR/bookkeeper-$command-$HOSTNAME.out
+logfile=$BOOKIE_LOG_DIR/$BOOKIE_LOG_FILE
+
+rotate_out_log ()
+{
+    log=$1;
+    num=5;
+    if [ -n "$2" ]; then
+       num=$2
+    fi
+    if [ -f "$log" ]; then # rotate logs
+        while [ $num -gt 1 ]; do
+            prev=`expr $num - 1`
+            [ -f "$log.$prev" ] && mv "$log.$prev" "$log.$num"
+            num=$prev
+        done
+        mv "$log" "$log.$num";
+    fi
+}
+
+mkdir -p "$BOOKIE_LOG_DIR"
+
+case $startStop in
+  (start)
+    if [ -f $pid ]; then
+      if kill -0 `cat $pid` > /dev/null 2>&1; then
+        echo $command running as process `cat $pid`.  Stop it first.
+        exit 1
+      fi
+    fi
+
+    rotate_out_log $out
+    echo starting $command, logging to $logfile
+    bookkeeper=$BK_HOME/bin/bookkeeper
+    nohup $bookkeeper $command "$@" > "$out" 2>&1 < /dev/null &
+    echo $! > $pid
+    sleep 1; head $out
+    sleep 2;
+    if ! ps -p $! > /dev/null ; then
+      exit 1
+    fi
+    ;;
+
+  (stop)
+    if [ -f $pid ]; then
+      TARGET_PID=`cat $pid`
+      if kill -0 $TARGET_PID > /dev/null 2>&1; then
+        echo stopping $command
+        kill $TARGET_PID
+
+        count=0
+        location=$BOOKIE_LOG_DIR
+        while ps -p $TARGET_PID > /dev/null;
+         do
+          echo "Shutdown is in progress... Please wait..."
+          sleep 1
+          count=`expr $count + 1`
+         
+          if [ "$count" = "$BOOKIE_STOP_TIMEOUT" ]; then
+                break
+          fi
+         done
+        
+        if [ "$count" != "$BOOKIE_STOP_TIMEOUT" ]; then
+                 echo "Shutdown completed."
+                exit 0
+        fi
+                 
+        if kill -0 $TARGET_PID > /dev/null 2>&1; then
+              fileName=$location/$command.out
+              $JAVA_HOME/bin/jstack $TARGET_PID > $fileName
+              echo Thread dumps are taken for analysis at $fileName
+              if [ "$1" == "-force" ]
+              then
+                 echo forcefully stopping $command
+                 kill -9 $TARGET_PID >/dev/null 2>&1
+                 echo Successfully stopped the process
+              else
+                 echo "WARNNING :  Bookie Server is not stopped completely."
+                 exit 1
+              fi
+        fi
+      else
+        echo no $command to stop
+      fi
+      rm $pid
+    else
+      echo no $command to stop
+    fi
+    ;;
+
+  (*)
+    echo $usage
+    echo $supportedargs
+    exit 1
+    ;;
+esac
