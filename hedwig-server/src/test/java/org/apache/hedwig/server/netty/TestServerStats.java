7a4b8171447f1a4aa056c36970c0a38625ecbb78
Flavio Paiva Junqueira <fpj@apache.org>
2012-07-27 15:07:56 +0000

    BOOKKEEPER-352: Should not use static ServerStats/BKStats instance in TestServerStats/TestBKStats (sijie via fpj)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/bookkeeper/branches/branch-4.1@1366409 13f79535-47bb-0310-9956-ffa450edef68

--- hedwig-server/src/test/java/org/apache/hedwig/server/netty/TestServerStats.java
+++ hedwig-server/src/test/java/org/apache/hedwig/server/netty/TestServerStats.java
@@ -2,7 +2,7 @@ package org.apache.hedwig.server.netty;
 
 import static org.junit.Assert.assertEquals;
 
-import org.apache.hedwig.protocol.PubSubProtocol.OperationType;
+import org.apache.hedwig.server.netty.ServerStats.OpStats;
 import org.junit.Test;
 
 /** Tests that Statistics updation in hedwig Server */
@@ -15,9 +15,7 @@ public class TestServerStats {
     @Test
     public void testUpdateLatencyShouldNotFailWithAIOBEWithNegativeLatency()
             throws Exception {
-        ServerStats stats = ServerStats.getInstance();
-        org.apache.hedwig.server.netty.ServerStats.OpStats opStat = stats
-                .getOpStats(OperationType.SUBSCRIBE);
+        OpStats opStat = new OpStats();
         opStat.updateLatency(-10);
         assertEquals("Should not update any latency metrics", 0,
                 opStat.numSuccessOps);
