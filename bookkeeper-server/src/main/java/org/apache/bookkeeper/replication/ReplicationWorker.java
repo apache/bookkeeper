f41bb6f8b521e26a0a746530e648a9ca2120af51
Ivan Brendan Kelly <ivank@apache.org>
2013-08-20 11:02:59 +0000

    BOOKKEEPER-624: Reduce logs generated by ReplicationWorker (vinay via ivank)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/bookkeeper/trunk@1515774 13f79535-47bb-0310-9956-ffa450edef68

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/replication/ReplicationWorker.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/replication/ReplicationWorker.java
@@ -138,8 +138,7 @@ public class ReplicationWorker implements Runnable {
             UnavailableException {
         long ledgerIdToReplicate = underreplicationManager
                 .getLedgerToRereplicate();
-        LOG.info("Going to replicate the fragments of the ledger: "
-                + ledgerIdToReplicate);
+        LOG.debug("Going to replicate the fragments of the ledger: {}", ledgerIdToReplicate);
         LedgerHandle lh;
         try {
             lh = admin.openLedgerNoRecovery(ledgerIdToReplicate);
@@ -169,8 +168,7 @@ public class ReplicationWorker implements Runnable {
             return;
         }
         Set<LedgerFragment> fragments = getUnderreplicatedFragments(lh);
-        LOG.info("Founds fragments " + fragments
-                + " for replication from ledger: " + ledgerIdToReplicate);
+        LOG.debug("Founds fragments {} for replication from ledger: {}", fragments, ledgerIdToReplicate);
 
         boolean foundOpenFragments = false;
         for (LedgerFragment ledgerFragment : fragments) {
@@ -179,9 +177,8 @@ public class ReplicationWorker implements Runnable {
                 continue;
             } else if (isTargetBookieExistsInFragmentEnsemble(lh,
                     ledgerFragment)) {
-                LOG.info("Target Bookie[" + targetBookie
-                        + "] found in the fragment ensemble:"
-                        + ledgerFragment.getEnsemble());
+                LOG.debug("Target Bookie[{}] found in the fragment ensemble: {}", targetBookie,
+                        ledgerFragment.getEnsemble());
                 continue;
             }
             try {
