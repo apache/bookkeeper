c9db762707538a99a92852e60b88bed577b01377
Ivan Brendan Kelly <ivank@apache.org>
2014-06-10 10:01:45 +0000

    BOOKKEEPER-765 bookkeeper script should fall back to java in path if JAVA_HOME is not set (ivank)
    git-svn-id: https://svn.apache.org/repos/asf/zookeeper/bookkeeper/trunk@1601592 13f79535-47bb-0310-9956-ffa450edef68

--- hedwig-server/conf/hwenv.sh
+++ hedwig-server/conf/hwenv.sh
@@ -20,6 +20,9 @@
 # * limitations under the License.
 # */
 
+# Set JAVA_HOME here to override the environment setting
+# JAVA_HOME=
+
 # default settings for starting hedwig
 # HEDWIG_SERVER_CONF=
 
