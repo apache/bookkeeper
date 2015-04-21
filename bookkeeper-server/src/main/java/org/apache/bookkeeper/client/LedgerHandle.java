70da24d504e3f4cba0d87a0914a0add7ea19394d
Sijie Guo <sijie@apache.org>
2015-04-21 00:39:31 -0700

    BOOKKEEPER-848: Use volatile for lastAddConfirmed (Matteo Merli via sijie)

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/client/LedgerHandle.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/client/LedgerHandle.java
@@ -64,7 +64,7 @@ public class LedgerHandle {
     final BookKeeper bk;
     final long ledgerId;
     long lastAddPushed;
-    long lastAddConfirmed;
+    volatile long lastAddConfirmed;
     long length;
     final DigestManager macManager;
     final DistributionSchedule distributionSchedule;
