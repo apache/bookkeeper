92722ee9c34b069e23d1a87d7fc78256b8540268
Sijie Guo <sijie@apache.org>
2016-01-26 13:11:04 -0800

    BOOKKEEPER-855: handle session expire event in bookie (sijie)
    This change is to retry bookie registration when zookeeper session expired.
    
    Author: Sijie Guo <sijie@apache.org>
    
    Reviewers: Ivan Kelly <ivank@apache.org>, Matteo Merli <mmerli@apache.org>
    
    Closes #1 from sijie/sijie/BOOKKEEPER-855

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/test/ReadOnlyBookieTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/test/ReadOnlyBookieTest.java
@@ -248,7 +248,7 @@ public class ReadOnlyBookieTest extends BookKeeperClusterTestCase {
         killBookie(1);
         baseConf.setReadOnlyModeEnabled(true);
         startNewBookie();
-        bs.get(1).getBookie().transitionToReadOnlyMode();
+        bs.get(1).getBookie().doTransitionToReadOnlyMode();
         try {
             bkc.readBookiesBlocking();
             bkc.createLedger(2, 2, DigestType.CRC32, "".getBytes());
