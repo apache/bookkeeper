3196448632fd93b53964d58aaf070709d2bb050c
Franck Cuny <franckcuny@gmail.com>
2016-07-11 11:54:15 -0700

    A few changes to the shell scripts to start bookies
    Author: Franck Cuny <franckcuny@gmail.com>
    
    Reviewers: Sijie Guo <sijie@apache.org>
    
    Closes #43 from franckcuny/fcuny/scripts-start-server

--- bookkeeper-server/bin/bookkeeper-daemon.sh
+++ bookkeeper-server/bin/bookkeeper-daemon.sh
@@ -29,8 +29,8 @@ where argument is one of:
 EOF
 }
 
-BINDIR=`dirname "$0"`
-BK_HOME=`cd $BINDIR/..;pwd`
+BINDIR=$(dirname "$0")
+BK_HOME=$(cd $BINDIR/..;pwd)
 
 if [ -f $BK_HOME/conf/bkenv.sh ]
 then
@@ -47,9 +47,9 @@ BOOKIE_PID_DIR=${BOOKIE_PID_DIR:-$BK_HOME/bin}
 
 if [ $# -lt 2 ]
 then
-    echo "Error: no enough arguments provided."
-    usage
-    exit 1
+  echo "Error: no enough arguments provided."
+  usage
+  exit 1
 fi
 
 startStop=$1
@@ -58,51 +58,52 @@ command=$1
 shift
 
 case $command in
-    (bookie)
-        echo "doing $startStop $command ..."
-        ;;
-    (autorecovery)
-        echo "doing $startStop $command ..."
-        ;;
-    (*)
-        echo "Error: unknown service name $command"
-        usage
-        exit 1
-        ;;
+  (bookie)
+    echo "doing $startStop $command ..."
+    ;;
+  (autorecovery)
+    echo "doing $startStop $command ..."
+    ;;
+  (*)
+    echo "Error: unknown service name $command"
+    usage
+    exit 1
+    ;;
 esac
 
 export BOOKIE_LOG_DIR=$BOOKIE_LOG_DIR
 export BOOKIE_ROOT_LOGGER=$BOOKIE_ROOT_LOGGER
 export BOOKIE_LOG_FILE=bookkeeper-$command-$HOSTNAME.log
 
-pid=$BOOKIE_PID_DIR/bookkeeper-$command.pid
+pid_file="${BOOKIE_PID_DIR}/bookkeeper-${command}.pid"
 out=$BOOKIE_LOG_DIR/bookkeeper-$command-$HOSTNAME.out
 logfile=$BOOKIE_LOG_DIR/$BOOKIE_LOG_FILE
 
 rotate_out_log ()
 {
-    log=$1;
-    num=5;
-    if [ -n "$2" ]; then
-       num=$2
-    fi
-    if [ -f "$log" ]; then # rotate logs
-        while [ $num -gt 1 ]; do
-            prev=`expr $num - 1`
-            [ -f "$log.$prev" ] && mv "$log.$prev" "$log.$num"
-            num=$prev
-        done
-        mv "$log" "$log.$num";
-    fi
+  log=$1;
+  num=5;
+  if [ -n "$2" ]; then
+    num=$2
+  fi
+  if [ -f "$log" ]; then # rotate logs
+    while [ $num -gt 1 ]; do
+      prev=$(expr $num - 1)
+      [ -f "$log.$prev" ] && mv "$log.$prev" "$log.$num"
+      num=$prev
+    done
+    mv "$log" "$log.$num";
+  fi
 }
 
 mkdir -p "$BOOKIE_LOG_DIR"
 
 case $startStop in
   (start)
-    if [ -f $pid ]; then
-      if kill -0 `cat $pid` > /dev/null 2>&1; then
-        echo $command running as process `cat $pid`.  Stop it first.
+    if [ -f $pid_file ]; then
+      PREVIOUS_PID=$(cat $pid_file)
+      if kill -0 $PREVIOUS_PID > /dev/null 2>&1; then
+        echo $command running as process $PREVIOUS_PID.  Stop it first.
         exit 1
       fi
     fi
@@ -111,56 +112,56 @@ case $startStop in
     echo starting $command, logging to $logfile
     bookkeeper=$BK_HOME/bin/bookkeeper
     nohup $bookkeeper $command "$@" > "$out" 2>&1 < /dev/null &
-    echo $! > $pid
+    echo $! > $pid_file
     sleep 1; head $out
     sleep 2;
-    if ! ps -p $! > /dev/null ; then
+    if ! kill -0 $! > /dev/null ; then
       exit 1
     fi
     ;;
 
   (stop)
-    if [ -f $pid ]; then
-      TARGET_PID=`cat $pid`
+    if [ -f $pid_file ]; then
+      TARGET_PID=$(cat $pid_file)
       if kill -0 $TARGET_PID > /dev/null 2>&1; then
         echo stopping $command
         kill $TARGET_PID
 
         count=0
         location=$BOOKIE_LOG_DIR
-        while ps -p $TARGET_PID > /dev/null;
-         do
+        while kill -0 $TARGET_PID > /dev/null;
+        do
           echo "Shutdown is in progress... Please wait..."
           sleep 1
-          count=`expr $count + 1`
-         
+          count=$(expr $count + 1)
+
           if [ "$count" = "$BOOKIE_STOP_TIMEOUT" ]; then
-                break
+            break
           fi
          done
-        
+
         if [ "$count" != "$BOOKIE_STOP_TIMEOUT" ]; then
-            echo "Shutdown completed."
+          echo "Shutdown completed."
         fi
-                 
+
         if kill -0 $TARGET_PID > /dev/null 2>&1; then
-              fileName=$location/$command.out
-              $JAVA_HOME/bin/jstack $TARGET_PID > $fileName
-              echo Thread dumps are taken for analysis at $fileName
-              if [ "$1" == "-force" ]
-              then
-                 echo forcefully stopping $command
-                 kill -9 $TARGET_PID >/dev/null 2>&1
-                 echo Successfully stopped the process
-              else
-                 echo "WARNNING :  Bookie Server is not stopped completely."
-                 exit 1
-              fi
+          fileName=$location/$command.out
+          $JAVA_HOME/bin/jstack $TARGET_PID > $fileName
+          echo Thread dumps are taken for analysis at $fileName
+          if [ "$1" == "-force" ]
+          then
+            echo forcefully stopping $command
+            kill -9 $TARGET_PID >/dev/null 2>&1
+            echo Successfully stopped the process
+          else
+            echo "WARNNING :  Bookie Server is not stopped completely."
+            exit 1
+          fi
         fi
       else
         echo no $command to stop
       fi
-      rm $pid
+      rm $pid_file
     else
       echo no $command to stop
     fi
