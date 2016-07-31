8324632b2983beba1528ab53a999340756c7f563
eolivelli <eolivelli@gmail.com>
2016-07-30 22:53:28 -0700

    BOOKKEEPER-933: ClientConfiguration always inherits System properties
    Author: eolivelli <eolivelli@gmail.com>
    
    Reviewers: Sijie Guo <sijie@apache.org>
    
    Closes #50 from eolivelli/BOOKKEEPER-933 and squashes the following commits:
    
    0bcae1e [eolivelli] BOOKKEEPER-933 ClientConfiguration always inherits System properties
    beb68fb [eolivelli] BOOKKEEPER-933 ClientConfiguration always inherits System properties

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/conf/AbstractConfiguration.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/conf/AbstractConfiguration.java
@@ -37,6 +37,13 @@ import org.slf4j.LoggerFactory;
 public abstract class AbstractConfiguration extends CompositeConfiguration {
 
     static final Logger LOG = LoggerFactory.getLogger(AbstractConfiguration.class);
+    public static final String READ_SYSTEM_PROPERTIES_PROPERTY
+                            = "org.apache.bookkeeper.conf.readsystemproperties";
+    /**
+     * Enable the use of System Properties, which was the default behaviour till 4.4.0
+     */
+    private static final boolean READ_SYSTEM_PROPERTIES
+                                    = Boolean.getBoolean(READ_SYSTEM_PROPERTIES_PROPERTY);
 
     protected static final ClassLoader defaultLoader;
     static {
@@ -60,8 +67,10 @@ public abstract class AbstractConfiguration extends CompositeConfiguration {
 
     protected AbstractConfiguration() {
         super();
-        // add configuration for system properties
-        addConfiguration(new SystemConfiguration());
+        if (READ_SYSTEM_PROPERTIES) {
+            // add configuration for system properties
+            addConfiguration(new SystemConfiguration());
+        }
     }
 
     /**
--- bookkeeper-server/src/test/java/org/apache/bookkeeper/test/ConfigurationTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/test/ConfigurationTest.java
@@ -20,6 +20,7 @@
  */
 package org.apache.bookkeeper.test;
 
+import org.apache.bookkeeper.conf.AbstractConfiguration;
 import org.apache.bookkeeper.conf.ServerConfiguration;
 import org.apache.bookkeeper.conf.ClientConfiguration;
 
@@ -28,6 +29,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ConfigurationTest {
+
+    static {
+        // this property is read when AbstractConfiguration class is loaded.
+        // this test will work as expected only using a new JVM (or classloader) for the test
+        System.setProperty(AbstractConfiguration.READ_SYSTEM_PROPERTIES_PROPERTY, "true");
+    }
+
     @Test(timeout=60000)
     public void testConfigurationOverwrite() {
         System.clearProperty("zkServers");
