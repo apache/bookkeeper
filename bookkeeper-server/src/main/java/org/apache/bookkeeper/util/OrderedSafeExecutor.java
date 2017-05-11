56935880f4c1e40c7f4cd666df4ba3bf027afdb7
Matteo Merli <mmerli@apache.org>
2017-05-11 10:06:55 -0700

    BOOKKEEPER-1065: OrderedSafeExecutor should only have 1 thread per bucket

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/util/OrderedSafeExecutor.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/util/OrderedSafeExecutor.java
@@ -183,6 +183,7 @@ public class OrderedSafeExecutor {
                         .setNameFormat(name + "-orderedsafeexecutor-" + i + "-%d")
                         .setThreadFactory(threadFactory)
                         .build());
+            threads[i].setMaximumPoolSize(1);
 
             // Save thread ids
             final int idx = i;
