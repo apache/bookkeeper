9d8a2d7a4ba6bc5b8030210c1b327b85c14c362d
Enrico Olivelli <eolivelli@apache.org>
2017-07-27 09:29:15 +0200

    restore log message

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/util/LocalBookKeeper.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/util/LocalBookKeeper.java
@@ -297,6 +297,10 @@ public class LocalBookKeeper {
                 }
                 throw ie;
             }
+        } catch (Exception e) {
+                LOG.error("Failed to run {} bookies : zk ensemble = '{}:{}'",
+                    new Object[] { numBookies, zkHost, zkPort, e });
+                throw e;
         } finally {
             if (stopOnExit) {
                 cleanupDirectories(bkTmpDirs);
