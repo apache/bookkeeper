caba6b1dbd7eab07c8c6dd518797c55739f3524e
Sijie Guo <sijie@apache.org>
2017-08-11 15:27:27 -0700

    ISSUE #435: Build javadoc for releases
    Descriptions of the changes in this PR:
    
    - add javadoc links on sidebar and overview pages (try to promote this link for users to find the link easier)
    - update javadoc-gen.sh to be able to generate latest javadoc and all javadocs for versions under `docs`
    
    minor fixes:
    - pass "JEKYLL_ENV=production" to docker because ci uses docker to build the documentation
    -
    
    Author: Sijie Guo <sijie@apache.org>
    
    Reviewers: Matteo Merli <mmerli@apache.org>
    
    This closes #438 from sijie/publish_javadocs, closes #435

--- site/scripts/javadoc-gen.sh
+++ site/scripts/javadoc-gen.sh
@@ -2,9 +2,42 @@
 
 source scripts/common.sh
 
-(
-  rm -rf $JAVADOC_GEN_DIR $JAVADOC_DEST_DIR
-  cd $ROOT_DIR
+function build_javadoc() {
+  version=$1
+
+  echo "Building the javadoc for version ${version}."
+  if [ "$version" == "latest" ]; then
+    javadoc_gen_dir="${ROOT_DIR}/target/site/apidocs"
+    cd $ROOT_DIR
+  else
+    javadoc_gen_dir="/tmp/bookkeeper-${version}/target/site/apidocs"
+    rm -rf /tmp/bookkeeper-${version}
+    git clone https://github.com/apache/bookkeeper /tmp/bookkeeper-${version}
+    cd /tmp/bookkeeper-${version}
+    git checkout "release-${version}"
+  fi
+  javadoc_dest_dir="${ROOT_DIR}/site/docs/${version}/api/javadoc"
+  rm -rf $javadoc_dest_dir
   mvn clean install javadoc:aggregate -DskipTests
-  mv $JAVADOC_GEN_DIR $JAVADOC_DEST_DIR
-)
+  mv $javadoc_gen_dir $javadoc_dest_dir
+
+  echo "Built the javadoc for version ${version}."
+}
+
+# get arguments
+version=$1
+shift
+
+case "${version}" in
+  latest)
+    build_javadoc ${version}
+    ;;
+  all)
+    for d in `ls ${ROOT_DIR}/site/docs`; do
+      build_javadoc $d
+    done
+    ;;
+  *)
+    echo "Unknown version '${version}' to build doc"
+    ;;
+esac
