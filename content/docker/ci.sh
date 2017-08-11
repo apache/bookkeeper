9df249c50a42905ddcd510a80107bd53d14e0bc3
jenkins <builds@apache.org>
2017-08-11 10:26:06 +0000

    Updated site at revision b4a1c76

--- content/docker/ci.sh
+++ content/docker/ci.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -e -x -u
 
@@ -41,7 +57,6 @@ docker run \
   -v "${BOOKKEEPER_DOC_ROOT}:${BOOKKEEPER_DOC_ROOT}" \
   -v "${LOCAL_HOME}:/home/${USER_NAME}" \
   -p 4000:4000 \
-  -e JEKYLL_ENV=production \
   ${IMAGE_NAME}-${USER_NAME} \
   bash -c "make setup && make apache"
 
