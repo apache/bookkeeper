7406bcaf16b2606b5dfd21726fd45c67b821ed10
Jia Zhai <zhaijia@apache.org>
2017-10-27 14:33:41 +0800

    change following comments

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieInitializationTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieInitializationTest.java
@@ -81,7 +81,6 @@ public class BookieInitializationTest extends BookKeeperClusterTestCase {
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        LOG.info("setUp");
         zkUtil.createBKEnsemble("/" + runtime.getMethodName());
         rm = new ZKRegistrationManager();
     }
