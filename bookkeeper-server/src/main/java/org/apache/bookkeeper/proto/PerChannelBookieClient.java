0cd6d8cfe11782da35970a0fa180dfc82feb438c
Ivan Kelly <ivank@apache.org>
2017-12-01 19:00:04 +0100

    Make multiv2 changes fit with refactored PCBC

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/PerChannelBookieClient.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/PerChannelBookieClient.java
@@ -159,8 +159,8 @@ public class PerChannelBookieClient extends ChannelInboundHandlerAdapter {
 
     // Map that hold duplicated read requests. The idea is to only use this map (synchronized) when there is a duplicate
     // read request for the same ledgerId/entryId
-    private final ListMultimap<CompletionKey, CompletionValue> completionObjectsV2Conflicts = LinkedListMultimap
-            .create();
+    private final ListMultimap<CompletionKey, CompletionValue> completionObjectsV2Conflicts
+        = LinkedListMultimap.create();
 
     private final StatsLogger statsLogger;
     private final OpStatsLogger readEntryOpLogger;
@@ -615,7 +615,7 @@ public class PerChannelBookieClient extends ChannelInboundHandlerAdapter {
         CompletionValue existingValue = completionObjects.putIfAbsent(completionKey, readCompletion);
         if (existingValue != null) {
             // There's a pending read request on same ledger/entry. Use the multimap to track all of them
-            synchronized (this) {
+            synchronized (completionObjectsV2Conflicts) {
                 completionObjectsV2Conflicts.put(completionKey, readCompletion);
             }
         }
@@ -736,15 +736,9 @@ public class PerChannelBookieClient extends ChannelInboundHandlerAdapter {
         CompletionValue existingValue = completionObjects.putIfAbsent(completionKey, readCompletion);
         if (existingValue != null) {
             // There's a pending read request on same ledger/entry. Use the multimap to track all of them
-            synchronized (this) {
+            synchronized (completionObjectsV2Conflicts) {
                 completionObjectsV2Conflicts.put(completionKey, readCompletion);
             }
-        }
-
-        final Channel c = channel;
-        if (c == null) {
-            errorOutReadKey(completionKey);
-            return;
         }
 
         writeAndFlush(channel, completionKey, request);
@@ -849,56 +843,6 @@ public class PerChannelBookieClient extends ChannelInboundHandlerAdapter {
             errorOut(key);
         }
     }
-    
-    void errorOutReadKey(final CompletionKey key) {
-        errorOutReadKey(key, BKException.Code.BookieHandleNotAvailableException);
-    }
-
-    void errorOutReadKey(final CompletionKey key, final int rc) {
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("Removing completion key: {}", key);
-        }
-        ReadCompletion readCompletion = (ReadCompletion) completionObjects.remove(key);
-        if (readCompletion == null) {
-            // If there's no completion object here, try in the multimap
-            synchronized (this) {
-                if (completionObjectsV2Conflicts.containsKey(key)) {
-                    readCompletion = (ReadCompletion) completionObjectsV2Conflicts.get(key).get(0);
-                    completionObjectsV2Conflicts.remove(key, readCompletion);
-                }
-            }
-        }
-
-        // If it's still null, give up
-        if (null == readCompletion) {
-            return;
-        }
-        
-        final ReadCompletion finalReadCompletion = readCompletion;
-        executor.submitOrdered(readCompletion.ledgerId, new SafeRunnable() {
-            @Override
-            public void safeRun() {
-                String bAddress = "null";
-                Channel c = channel;
-                if (c != null && c.remoteAddress() != null) {
-                    bAddress = c.remoteAddress().toString();
-                }
-
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug("Could not write request for reading entry: {} ledger-id: {} bookie: {} rc: {}",
-                            finalReadCompletion.entryId, finalReadCompletion.ledgerId, bAddress, rc);
-                }
-
-                finalReadCompletion.cb.readEntryComplete(rc, finalReadCompletion.ledgerId, finalReadCompletion.entryId,
-                        null, finalReadCompletion.ctx);
-            }
-
-            @Override
-            public String toString() {
-                return String.format("ErrorOutReadKey(%s)", key);
-            }
-        });
-    }
 
     private static String requestToString(Object request) {
         if (request instanceof BookkeeperProtocol.Request) {
@@ -928,6 +872,16 @@ public class PerChannelBookieClient extends ChannelInboundHandlerAdapter {
         CompletionValue completion = completionObjects.remove(key);
         if (completion != null) {
             completion.errorOut(rc);
+        } else {
+            // If there's no completion object here, try in the multimap
+            synchronized (completionObjectsV2Conflicts) {
+                if (completionObjectsV2Conflicts.containsKey(key)) {
+                    completion = completionObjectsV2Conflicts.get(key).get(0);
+                    completionObjectsV2Conflicts.remove(key, completion);
+
+                    completion.errorOut(rc);
+                }
+            }
         }
     }
 
@@ -1072,13 +1026,10 @@ public class PerChannelBookieClient extends ChannelInboundHandlerAdapter {
             long orderingKey = completionValue.ledgerId;
             final CompletionValue finalCompletionValue = completionValue;
 
-            executor.submitOrdered(orderingKey, new SafeRunnable() {
-                @Override
-                public void safeRun() {
+            executor.submitOrdered(orderingKey, () -> {
                     finalCompletionValue.handleV2Response(ledgerId, entryId, status, response);
                     response.recycle();
-                }
-            });
+                });
         }
     }
 
