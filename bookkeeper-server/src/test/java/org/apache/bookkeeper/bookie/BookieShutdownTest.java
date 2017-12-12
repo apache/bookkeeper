13c303f916b5b82702d73e2377b2caf75500243a
Aaron Coburn <acoburn@amherst.edu>
2017-12-12 13:21:42 +0100

    ISSUE #230: Enable checkstyle on the bookie tests
    Descriptions of the changes in this PR:
    
    With this change, checkstyle verification is enabled for the
    entire bookkeeper-server package, and all packages use the same `suppressions.xml` file.
    
    Master Issue: #230
    
    Author: Aaron Coburn <acoburn@amherst.edu>
    
    Reviewers: Enrico Olivelli <eolivelli@gmail.com>, Jia Zhai <None>
    
    This closes #830 from acoburn/checkstyle_test_bookie, closes #230

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieShutdownTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/BookieShutdownTest.java
@@ -20,25 +20,29 @@
  */
 package org.apache.bookkeeper.bookie;
 
+import static org.junit.Assert.fail;
+
 import java.nio.ByteBuffer;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import org.apache.bookkeeper.client.BKException;
-import org.apache.bookkeeper.client.LedgerHandle;
+
 import org.apache.bookkeeper.client.AsyncCallback.AddCallback;
+import org.apache.bookkeeper.client.BKException;
 import org.apache.bookkeeper.client.BookKeeper.DigestType;
+import org.apache.bookkeeper.client.LedgerHandle;
 import org.apache.bookkeeper.conf.ServerConfiguration;
 import org.apache.bookkeeper.test.BookKeeperClusterTestCase;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.Assert.*;
-
+/**
+ * Test bookie shutdown.
+ */
 public class BookieShutdownTest extends BookKeeperClusterTestCase {
 
-    private final static Logger LOG = LoggerFactory.getLogger(BookieShutdownTest.class);
+    private static final Logger LOG = LoggerFactory.getLogger(BookieShutdownTest.class);
 
     public BookieShutdownTest() {
         super(3);
