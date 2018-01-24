3bf153b6c6620e25ddada65b7d862427ffaf8250
Ivan Kelly <ivank@apache.org>
2018-01-24 13:15:24 +0100

    Fix checkstyle

--- tests/backward/src/test/java/org/apache/bookkeeper/tests/backward/TestBackwardCompat.java
+++ tests/backward/src/test/java/org/apache/bookkeeper/tests/backward/TestBackwardCompat.java
@@ -20,7 +20,6 @@
  */
 package org.apache.bookkeeper.tests.backward;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
