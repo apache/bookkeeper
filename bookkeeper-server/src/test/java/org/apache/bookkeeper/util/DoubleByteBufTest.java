81e8d520062260882a03672636d74c408d0c44b9
Matteo Merli <mmerli@apache.org>
2018-02-15 15:27:33 -0800

    Replace DoubleByteBuf with CompositeByteBuf because of perf regression with Netty > 4.1.12
    Starting from Netty-4.1.13 some internal behavior has changed and that has introduced a performance penalty when using the `DoubleByteBuf`.
    
    The problem resides in the fact that netty falls back to calling `nioBuffers()` and that is creating an array of unpooled direct `ByteBuffers`. That is pretty heavy and the JVM is taking 1 to 3 seconds GC pause to reclaim them.
    
    Short term fix is to go back to `CompositeByteBuf` which doesn't have the problem (the `DirectByteBuffer` instances are pooled and reused). I have verified that GC is back to normal after this patch.
    
    More mid-term, since `CompositeByteBuf` allocates a bunch of objects (counted ~10 per entry) I have a change to replace it with a `ByteBufList` holder class. I have the change almost done but it will take a while to test it to verify corner cases.
    
    This should be considered for a 4.6.2 fix release.
    
    Author: Matteo Merli <mmerli@apache.org>
    
    Reviewers: Ivan Kelly <ivank@apache.org>, Enrico Olivelli <eolivelli@gmail.com>, Jia Zhai <None>, Sijie Guo <sijie@apache.org>, Venkateswararao Jujjuri (JV) <None>
    
    This closes #1108 from merlimat/composite-byte-buf-master
    
    (cherry picked from commit 1b8ad9cba38ba7ecbfbe3836c6db98a8b319e094)
    Signed-off-by: Matteo Merli <mmerli@apache.org>

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/util/DoubleByteBufTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/util/DoubleByteBufTest.java
@@ -166,8 +166,6 @@ public class DoubleByteBufTest {
         assertTrue(b1.isDirect());
         assertTrue(b2.isDirect());
         assertTrue(b.isDirect());
-        ByteBuffer nioBuffer = b.nioBuffer();
-        assertTrue(nioBuffer.isDirect());
     }
 
     /**
