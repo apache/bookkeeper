4d47418a5bfc186385a1adadeff55e7b8b19fbe3
Sijie Guo <sijie@apache.org>
2018-02-17 14:36:13 +0800

    BP-29 (task 1) : Introduce `metadataServiceUrl`
    Descriptions of the changes in this PR:
    
    - introduce a new setting `metadataServiceUrl` for replacing implementation specific settings `zkServers` and `zkLedgersRootPath`.
    - introduce a metadata driver interface for managing all the metadata interface
    - introduce a metadata driver manager for resolving `metadataServiceUrl` and load specific metadata service url based on service scheme. This
      would replace configuring multiple different manager classes using one single `metadataServiceUrl`.
    
    Please NOTE: this change only introduce the new classes, but it doesn't change the client and bookie to use the new setting. It will be done in
    a subsequent change.
    
    Master Issue: #1123
    
    Author: Sijie Guo <sijie@apache.org>
    
    Reviewers: Enrico Olivelli <eolivelli@gmail.com>, Venkateswararao Jujjuri (JV) <None>
    
    This closes #1127 from sijie/introduce_service_uri

---
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/meta/MetadataClientDriver.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.bookkeeper.meta;
+
+import java.util.Optional;
+import java.util.concurrent.ScheduledExecutorService;
+import org.apache.bookkeeper.common.annotation.InterfaceAudience.LimitedPrivate;
+import org.apache.bookkeeper.common.annotation.InterfaceStability.Evolving;
+import org.apache.bookkeeper.conf.ClientConfiguration;
+import org.apache.bookkeeper.discover.RegistrationClient;
+import org.apache.bookkeeper.meta.exceptions.MetadataException;
+import org.apache.bookkeeper.stats.StatsLogger;
+
+/**
+ * Driver to manage all the metadata managers required by a bookkeeper client.
+ */
+@LimitedPrivate
+@Evolving
+public interface MetadataClientDriver extends AutoCloseable {
+
+    /**
+     * Initialize the metadata driver.
+     *
+     * @param conf configuration
+     * @param scheduler scheduler
+     * @param statsLogger stats logger
+     * @param ctx optional context object passed in for initialization.
+     *            currently it is an external zookeeper instance, which can
+     *            be used for zookeeper based metadata implementation.
+     * @return metadata driver
+     * @throws MetadataException when fail to initialize the client driver.
+     */
+    MetadataClientDriver initialize(ClientConfiguration conf,
+                                    ScheduledExecutorService scheduler,
+                                    StatsLogger statsLogger,
+                                    Optional<Object> ctx)
+        throws MetadataException;
+
+    /**
+     * Get the scheme of the metadata driver.
+     *
+     * @return the scheme of the metadata driver.
+     */
+    String getScheme();
+
+    /**
+     * Return the registration client used for discovering registered bookies.
+     *
+     * @return the registration client used for discovering registered bookies.
+     */
+    RegistrationClient getRegistrationClient()
+        throws MetadataException;
+
+    /**
+     * Return the ledger manager factory used for accessing ledger metadata.
+     *
+     * @return the ledger manager factory used for accessing ledger metadata.
+     */
+    LedgerManagerFactory getLedgerManagerFactory()
+        throws MetadataException;
+
+    @Override
+    void close();
+
+}
---
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/meta/MetadataDrivers.java
@@ -0,0 +1,290 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.bookkeeper.meta;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Sets;
+import java.net.URI;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.bookkeeper.util.ReflectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * A driver manager for managing a set of metadata drivers.
+ *
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Slf4j
+public final class MetadataDrivers {
+
+    static final String ZK_CLIENT_DRIVER_CLASS = "org.apache.bookkeeper.meta.zk.ZKMetadataClientDriver";
+    static final String ZK_BOOKIE_DRIVER_CLASS = "org.apache.bookkeeper.meta.zk.ZKMetadataBookieDriver";
+    static final String BK_METADATA_CLIENT_DRIVERS_PROPERTY = "bookkeeper.metadata.client.drivers";
+    static final String BK_METADATA_BOOKIE_DRIVERS_PROPERTY = "bookkeeper.metadata.bookie.drivers";
+
+    @ToString
+    static class MetadataClientDriverInfo {
+
+        final Class<? extends MetadataClientDriver> driverClass;
+        final String driverClassName;
+
+        MetadataClientDriverInfo(Class<? extends MetadataClientDriver> driverClass) {
+            this.driverClass = driverClass;
+            this.driverClassName = driverClass.getName();
+        }
+
+    }
+
+    @ToString
+    static class MetadataBookieDriverInfo {
+
+        final Class<? extends MetadataBookieDriver> driverClass;
+        final String driverClassName;
+
+        MetadataBookieDriverInfo(Class<? extends MetadataBookieDriver> driverClass) {
+            this.driverClass = driverClass;
+            this.driverClassName = driverClass.getName();
+        }
+
+    }
+
+    @Getter(AccessLevel.PACKAGE)
+    private static final ConcurrentMap<String, MetadataClientDriverInfo> clientDrivers;
+    @Getter(AccessLevel.PACKAGE)
+    private static final ConcurrentMap<String, MetadataBookieDriverInfo> bookieDrivers;
+    private static boolean initialized = false;
+
+    static {
+        clientDrivers = new ConcurrentHashMap<>();
+        bookieDrivers = new ConcurrentHashMap<>();
+        initialize();
+    }
+
+    static void initialize() {
+        if (initialized) {
+            return;
+        }
+        loadInitialDrivers();
+        initialized = true;
+        log.info("BookKeeper metadata driver manager initialized");
+    }
+
+    @VisibleForTesting
+    static void loadInitialDrivers() {
+        loadInitialClientDrivers();
+        loadInitialBookieDrivers();
+    }
+
+    private static void loadInitialClientDrivers() {
+        Set<String> driverList = Sets.newHashSet();
+
+        // add default zookeeper based driver
+        driverList.add(ZK_CLIENT_DRIVER_CLASS);
+
+        // load drivers from system property
+        String driversStr = System.getProperty(BK_METADATA_CLIENT_DRIVERS_PROPERTY);
+        if (null != driversStr) {
+            String[] driversArray = StringUtils.split(driversStr, ':');
+            for (String driver : driversArray) {
+                driverList.add(driver);
+            }
+        }
+
+        // initialize the drivers
+        for (String driverClsName : driverList) {
+            try {
+                MetadataClientDriver driver =
+                    ReflectionUtils.newInstance(driverClsName, MetadataClientDriver.class);
+                MetadataClientDriverInfo driverInfo =
+                    new MetadataClientDriverInfo(driver.getClass());
+                clientDrivers.put(driver.getScheme().toLowerCase(), driverInfo);
+            } catch (Exception e) {
+                log.warn("Failed to load metadata client driver {}", driverClsName, e);
+            }
+        }
+    }
+
+    private static void loadInitialBookieDrivers() {
+        Set<String> driverList = Sets.newHashSet();
+
+        // add default zookeeper based driver
+        driverList.add(ZK_BOOKIE_DRIVER_CLASS);
+
+        // load drivers from system property
+        String driversStr = System.getProperty(BK_METADATA_BOOKIE_DRIVERS_PROPERTY);
+        if (null != driversStr) {
+            String[] driversArray = StringUtils.split(driversStr, ':');
+            for (String driver : driversArray) {
+                driverList.add(driver);
+            }
+        }
+
+        // initialize the drivers
+        for (String driverClsName : driverList) {
+            try {
+                MetadataBookieDriver driver =
+                    ReflectionUtils.newInstance(driverClsName, MetadataBookieDriver.class);
+                MetadataBookieDriverInfo driverInfo =
+                    new MetadataBookieDriverInfo(driver.getClass());
+                bookieDrivers.put(driver.getScheme().toLowerCase(), driverInfo);
+            } catch (Exception e) {
+                log.warn("Failed to load metadata bookie driver {}", driverClsName, e);
+            }
+        }
+    }
+
+    /**
+     * Register the metadata client {@code driver}.
+     *
+     * @param metadataBackendScheme scheme of metadata backend.
+     * @param driver metadata client driver
+     */
+    public static void registerClientDriver(String metadataBackendScheme,
+                                            Class<? extends MetadataClientDriver> driver) {
+        if (!initialized) {
+            initialize();
+        }
+
+        String scheme = metadataBackendScheme.toLowerCase();
+        MetadataClientDriverInfo oldDriverInfo = clientDrivers.get(scheme);
+        if (null != oldDriverInfo) {
+            return;
+        }
+        MetadataClientDriverInfo newDriverInfo = new MetadataClientDriverInfo(driver);
+        oldDriverInfo = clientDrivers.putIfAbsent(scheme, newDriverInfo);
+        if (null != oldDriverInfo) {
+            log.debug("Metadata client driver for {} is already there.", scheme);
+        }
+    }
+
+    /**
+     * Register the metadata bookie {@code driver}.
+     *
+     * @param metadataBackendScheme scheme of metadata backend.
+     * @param driver metadata bookie driver
+     */
+    public static void registerBookieDriver(String metadataBackendScheme,
+                                            Class<? extends MetadataBookieDriver> driver) {
+        if (!initialized) {
+            initialize();
+        }
+
+        String scheme = metadataBackendScheme.toLowerCase();
+        MetadataBookieDriverInfo oldDriverInfo = bookieDrivers.get(scheme);
+        if (null != oldDriverInfo) {
+            return;
+        }
+        MetadataBookieDriverInfo newDriverInfo = new MetadataBookieDriverInfo(driver);
+        oldDriverInfo = bookieDrivers.putIfAbsent(scheme, newDriverInfo);
+        if (null != oldDriverInfo) {
+            log.debug("Metadata bookie driver for {} is already there.", scheme);
+        }
+    }
+
+    /**
+     * Retrieve the client driver for {@code scheme}.
+     *
+     * @param scheme the scheme for the metadata client driver
+     * @return the metadata client driver
+     * @throws NullPointerException when scheme is null
+     */
+    public static MetadataClientDriver getClientDriver(String scheme) {
+        checkNotNull(scheme, "Client Driver Scheme is null");
+        if (!initialized) {
+            initialize();
+        }
+        MetadataClientDriverInfo driverInfo = clientDrivers.get(scheme.toLowerCase());
+        if (null == driverInfo) {
+            throw new IllegalArgumentException("Unknown backend " + scheme);
+        }
+        return ReflectionUtils.newInstance(driverInfo.driverClass);
+    }
+
+    /**
+     * Retrieve the client driver for {@code uri}.
+     *
+     * @param uri the metadata service uri
+     * @return the metadata client driver for {@code uri}
+     * @throws NullPointerException if the metadata service {@code uri} is null or doesn't have scheme
+     *          or there is no namespace driver registered for the scheme
+     * @throws IllegalArgumentException if the metadata service {@code uri} scheme is illegal
+     */
+    public static MetadataClientDriver getClientDriver(URI uri) {
+        // Validate the uri and load the backend according to scheme
+        checkNotNull(uri, "Metadata service uri is null");
+        String scheme = uri.getScheme();
+        checkNotNull(scheme, "Invalid metadata service uri : " + uri);
+        scheme = scheme.toLowerCase();
+        String[] schemeParts = StringUtils.split(scheme, '+');
+        checkArgument(schemeParts.length > 0,
+                "Invalid metadata service scheme found : " + uri);
+        return getClientDriver(schemeParts[0]);
+    }
+
+    /**
+     * Retrieve the bookie driver for {@code scheme}.
+     *
+     * @param scheme the scheme for the metadata bookie driver
+     * @return the metadata bookie driver
+     * @throws NullPointerException when scheme is null
+     */
+    public static MetadataBookieDriver getBookieDriver(String scheme) {
+        checkNotNull(scheme, "Bookie Driver Scheme is null");
+        if (!initialized) {
+            initialize();
+        }
+        MetadataBookieDriverInfo driverInfo = bookieDrivers.get(scheme.toLowerCase());
+        if (null == driverInfo) {
+            throw new IllegalArgumentException("Unknown backend " + scheme);
+        }
+        return ReflectionUtils.newInstance(driverInfo.driverClass);
+    }
+
+    /**
+     * Retrieve the bookie driver for {@code uri}.
+     *
+     * @param uri the metadata service uri
+     * @return the metadata bookie driver for {@code uri}
+     * @throws NullPointerException if the metadata service {@code uri} is null or doesn't have scheme
+     *          or there is no namespace driver registered for the scheme
+     * @throws IllegalArgumentException if the metadata service {@code uri} scheme is illegal
+     */
+    public static MetadataBookieDriver getBookieDriver(URI uri) {
+        // Validate the uri and load the backend according to scheme
+        checkNotNull(uri, "Metadata service uri is null");
+        String scheme = uri.getScheme();
+        checkNotNull(scheme, "Invalid metadata service uri : " + uri);
+        scheme = scheme.toLowerCase();
+        String[] schemeParts = StringUtils.split(scheme, '+');
+        checkArgument(schemeParts.length > 0,
+                "Invalid metadata service scheme found : " + uri);
+        return getBookieDriver(schemeParts[0]);
+    }
+
+}
