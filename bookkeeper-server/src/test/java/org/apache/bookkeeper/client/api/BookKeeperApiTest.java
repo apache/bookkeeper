2fb96aa786f0521996c6ada44248102168b86401
Sijie Guo <sijie@apache.org>
2018-03-16 16:09:36 -0700

    fix checkstyle

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/api/BookKeeperApiTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/api/BookKeeperApiTest.java
@@ -31,7 +31,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.when;
 
 import io.netty.buffer.Unpooled;
 import java.nio.ByteBuffer;
