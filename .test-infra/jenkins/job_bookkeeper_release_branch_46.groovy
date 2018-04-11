8cd9f91bd49b0453e2ee5e787d5a5729255b466c
Sijie Guo <sijie@apache.org>
2018-04-10 22:47:57 -0700

    Create CI jobs for branch-4.7
    Descriptions of the changes in this PR:
    
    We need to CI jobs running for subsequent minor releases on branch-4.7.
    
    Author: Sijie Guo <sijie@apache.org>
    
    Reviewers: Enrico Olivelli <eolivelli@gmail.com>, Matteo Merli <mmerli@apache.org>
    
    This closes #1328 from sijie/create_ci_job_for_4.7

---
+++ .test-infra/jenkins/job_bookkeeper_release_branch_46.groovy
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import common_job_properties
+
+// This job runs nightly build for bookkeeper release branch-4.6
+mavenJob('bookkeeper_release_branch_46') {
+  description('Run nightly build for bookkeeper release branch-4.6')
+
+  // Set common parameters.
+  common_job_properties.setTopLevelMainJobProperties(
+      delegate,
+      "branch-4.6")
+
+  // Sets that this is a PostCommit job.
+  common_job_properties.setPostCommit(
+      delegate,
+      'H 12 * * *',
+      false)
+
+  // Allows triggering this build against pull requests.
+  common_job_properties.enablePhraseTriggeringFromPullRequest(
+      delegate,
+      'Release Branch 4.6 Test',
+      '/test-release-branch-46')
+  
+  // Set maven parameters.
+  common_job_properties.setMavenConfig(delegate)
+
+  // Maven build project.
+  goals('clean apache-rat:check package findbugs:check')
+}
