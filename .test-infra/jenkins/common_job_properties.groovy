6e75d3055be0d34330c48fb79d20a49d94abc018
Sijie Guo <sijie@apache.org>
2018-05-22 14:34:52 -0700

    Fix usernamePassword

--- .test-infra/jenkins/common_job_properties.groovy
+++ .test-infra/jenkins/common_job_properties.groovy
@@ -110,7 +110,7 @@ class common_job_properties {
 
       credentialsBinding {
         string("COVERALLS_REPO_TOKEN", "bookkeeper-coveralls-token")
-        usernamePassword(credentialsId: 'bookkeeper_dockerhub', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASSWORD')
+        usernamePassword('DOCKER_USER', 'DOCKER_PASSWORD', 'bookkeeper_dockerhub')
       }
     }
   }
