10c510333d904debf54eb832524f6e09dcc0685d
cguttapalem <cguttapalem@salesforce.com>
2018-06-19 18:08:26 -0700

    ISSUE #1495: Option to enforce minNumRacksPerWriteQuorum
    Descriptions of the changes in this PR:
    
    Provide an option to enforce the guarantee of minNumRacksPerWriteQuorum
    if it is enabled. If it cann't find a bookie to enforce the guarantee
    then the API in RackawareEnsemblePlacementPolicy should throw
    BKNotEnoughBookiesException.
    
    Master Issue: #1495
    
    Author: cguttapalem <cguttapalem@salesforce.com>
    
    Reviewers: Sijie Guo <sijie@apache.org>
    
    This closes #1496 from reddycharan/enforceminracks, closes #1495

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/client/RegionAwareEnsemblePlacementPolicy.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/client/RegionAwareEnsemblePlacementPolicy.java
@@ -131,7 +131,7 @@ public class RegionAwareEnsemblePlacementPolicy extends RackawareEnsemblePlaceme
                 perRegionPlacement.put(region, new RackawareEnsemblePlacementPolicy()
                         .initialize(dnsResolver, timer, this.reorderReadsRandom, this.stabilizePeriodSeconds,
                                 this.reorderThresholdPendingRequests, this.isWeighted, this.maxWeightMultiple,
-                                this.minNumRacksPerWriteQuorum, statsLogger)
+                                this.minNumRacksPerWriteQuorum, this.enforceMinNumRacksPerWriteQuorum, statsLogger)
                         .withDefaultRack(NetworkTopology.DEFAULT_REGION_AND_RACK));
             }
 
@@ -176,11 +176,11 @@ public class RegionAwareEnsemblePlacementPolicy extends RackawareEnsemblePlaceme
             // Regions are specified as
             // R1;R2;...
             String[] regions = regionsString.split(";");
-            for (String region: regions) {
+            for (String region : regions) {
                 perRegionPlacement.put(region, new RackawareEnsemblePlacementPolicy(true)
                         .initialize(dnsResolver, timer, this.reorderReadsRandom, this.stabilizePeriodSeconds,
                                 this.reorderThresholdPendingRequests, this.isWeighted, this.maxWeightMultiple,
-                                this.minNumRacksPerWriteQuorum, statsLogger)
+                                this.minNumRacksPerWriteQuorum, this.enforceMinNumRacksPerWriteQuorum, statsLogger)
                         .withDefaultRack(NetworkTopology.DEFAULT_REGION_AND_RACK));
             }
             minRegionsForDurability = conf.getInt(REPP_MINIMUM_REGIONS_FOR_DURABILITY,
@@ -502,7 +502,8 @@ public class RegionAwareEnsemblePlacementPolicy extends RackawareEnsemblePlaceme
                         bookieNodeToReplace.getNetworkLocation(),
                         excludeBookies,
                         TruePredicate.INSTANCE,
-                        EnsembleForReplacementWithNoConstraints.INSTANCE);
+                        EnsembleForReplacementWithNoConstraints.INSTANCE,
+                        true);
                 } catch (BKException.BKNotEnoughBookiesException e) {
                     LOG.warn("Failed to choose a bookie from {} : "
                             + "excluded {}, fallback to choose bookie randomly from the cluster.",
