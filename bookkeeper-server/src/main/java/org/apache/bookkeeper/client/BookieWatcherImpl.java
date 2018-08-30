dbf69684307204f40817bfbb1fd803599d7a1139
Qi Wang <codingwangqi@gmail.com>
2018-08-30 02:13:49 -0700

    BookieWatcherImpl doesn't initCause when throwing MetaStoreException

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/client/BookieWatcherImpl.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/client/BookieWatcherImpl.java
@@ -62,7 +62,9 @@ class BookieWatcherImpl implements BookieWatcher {
             log.error("Interrupted reading bookie list : ", cause);
             return new BKInterruptedException();
         } else {
-            return new MetaStoreException();
+            MetaStoreException mse = new MetaStoreException();
+            mse.initCause(cause);
+            return mse;
         }
     };
 
