d9b6cad21899fd36cb5ae27cd7d1fef4f7b2a1fa
Qi Wang <codingwangqi@gmail.com>
2018-08-31 23:49:42 -0700

    Address Enrico & Sijie's comments

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/client/LedgerOpenOp.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/client/LedgerOpenOp.java
@@ -31,9 +31,9 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import org.apache.bookkeeper.client.AsyncCallback.OpenCallback;
 import org.apache.bookkeeper.client.AsyncCallback.ReadLastConfirmedCallback;
-import org.apache.bookkeeper.client.BKException.Code;
 import org.apache.bookkeeper.client.BookKeeper.DigestType;
 import org.apache.bookkeeper.client.SyncCallbackUtils.SyncOpenCallback;
+import org.apache.bookkeeper.client.api.BKException.Code;
 import org.apache.bookkeeper.client.api.ReadHandle;
 import org.apache.bookkeeper.client.impl.OpenBuilderBase;
 import org.apache.bookkeeper.proto.BookkeeperInternalCallbacks.GenericCallback;
@@ -246,8 +246,9 @@ class LedgerOpenOp implements GenericCallback<LedgerMetadata> {
         }
 
         private void open(OpenCallback cb) {
-            if (!validate()) {
-                cb.openComplete(Code.IllegalOpException, null, null);
+            final int validateRc = validate();
+            if (Code.OK != validateRc) {
+                cb.openComplete(validateRc, null, null);
                 return;
             }
 
