296d6ffc8de9cb52447c72688294b57babdf2a1f
Sijie Guo <sijie@apache.org>
2018-09-04 13:12:17 -0700

    [tests] update bc tests to include 4.7.1 and 4.7.2
    *Motivation*
    
    We have released 4.7.1 and 4.7.2. We need BC test coverage on these two releases.

--- tests/backward-compat/hostname-bookieid/src/test/groovy/org/apache/bookkeeper/tests/backwardcompat/TestCompatUpgradeWithHostnameBookieId.groovy
+++ tests/backward-compat/hostname-bookieid/src/test/groovy/org/apache/bookkeeper/tests/backwardcompat/TestCompatUpgradeWithHostnameBookieId.groovy
@@ -40,7 +40,7 @@ class TestCompatUpgradeWithHostnameBookieId {
     private def oldClientVersions = ["4.1.0", "4.2.0", "4.2.1", "4.2.2", "4.2.3", "4.2.4",
                                      "4.3.0", "4.3.1", "4.3.2", "4.4.0", "4.5.0", "4.5.1",
                                      "4.6.0", "4.6.1", "4.6.2",
-                                     "4.7.0"]
+                                     "4.7.0", "4.7.1", "4.7.2"]
 
     @ArquillianResource
     DockerClient docker
