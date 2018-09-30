be69c9ab84d2ff4fdc9edb62c9b6de110d7918df
Sijie Guo <guosijie@gmail.com>
2018-09-30 11:39:08 -0700

    [TABLE SERVICE] [COMMON] add BytesHashRouter
    Descriptions of the changes in this PR:
    
    *Motivation*
    
    Currently the hash router takes `ByteBuf`. At some cases,
    we need to compute the hash of bytes array directly.
    
    *Changes*
    
    Add `BytesHashRouter` to compute hash value of bytes array directly,
    avoiding wrapping bytes into `ByteBuf`
    
    
    
    
    Author: Sijie Guo <guosijie@gmail.com>
    Author: Enrico Olivelli <eolivelli@gmail.com>
    Author: Sijie Guo <sijie@apache.org>
    
    Reviewers: Enrico Olivelli <eolivelli@gmail.com>
    
    This closes #1719 from sijie/bytes_hash_router

--- stream/common/src/test/java/org/apache/bookkeeper/common/router/HashRouterTest.java
+++ stream/common/src/test/java/org/apache/bookkeeper/common/router/HashRouterTest.java
@@ -15,6 +15,7 @@
 package org.apache.bookkeeper.common.router;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import com.google.common.hash.Hashing;
@@ -32,33 +33,41 @@ public class HashRouterTest {
 
     @Test
     public void testByteBufHashRouter() {
-        ByteBuf key = Unpooled.wrappedBuffer("foo".getBytes(UTF_8));
+        byte[] keyBytes = "foo".getBytes(UTF_8);
+        ByteBuf key = Unpooled.wrappedBuffer(keyBytes);
 
         // murmur3 - 32 bits
         int hash32 = Murmur3.hash32(
             key, key.readerIndex(), key.readableBytes(), (int) AbstractHashRouter.HASH_SEED);
+        int bytesHash32 = Murmur3.hash32(keyBytes, 0, keyBytes.length, (int) AbstractHashRouter.HASH_SEED);
         int guavaHash32 = Hashing.murmur3_32((int) AbstractHashRouter.HASH_SEED)
             .newHasher()
             .putString("foo", UTF_8)
             .hash()
             .asInt();
+        assertEquals(hash32, bytesHash32);
         assertEquals(hash32, guavaHash32);
 
         // murmur3 - 128 bits
         long[] hash128 = Murmur3.hash128(
             key, key.readerIndex(), key.readableBytes(), AbstractHashRouter.HASH_SEED);
-        log.info("hash128: {}", hash128);
+        long[] bytesHash128 = Murmur3.hash128(keyBytes, 0, keyBytes.length, AbstractHashRouter.HASH_SEED);
+        log.info("hash128: {}, bytes hash128: {}", hash128, bytesHash128);
         long guavaHash128 = Hashing.murmur3_128((int) AbstractHashRouter.HASH_SEED)
             .newHasher()
             .putString("foo", UTF_8)
             .hash()
             .asLong();
+        assertArrayEquals(hash128, bytesHash128);
         assertEquals(hash128[0], guavaHash128);
 
         ByteBufHashRouter router = ByteBufHashRouter.of();
         long routingHash = router.getRoutingKey(key).longValue();
         log.info("Routing hash = {}", routingHash);
         assertEquals(hash128[0], routingHash);
+        BytesHashRouter bytesRouter = BytesHashRouter.of();
+        long bytesRoutingHash = bytesRouter.getRoutingKey(keyBytes).longValue();
+        assertEquals(hash128[0], bytesRoutingHash);
     }
 
 }
