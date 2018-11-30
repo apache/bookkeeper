87c0abc5919e97329ab9d50dae3d9981ab21e756
Matteo Merli <mmerli@apache.org>
2018-11-30 00:27:23 -0800

    Set default sizes of DbLedgerStorage read and write cache to be proportional to JVM direct memory
    ### Motivation
    
    To simplify Bookie configuration when using `DbLedgerStorage`, set the memory size defaults for WriteCache, ReadCache and RocksDB block cache to be pegged to the available direct memory configured in the JVM.
    
    User can always configure specific values and override this behavior.
    
    
    Reviewers: Enrico Olivelli <eolivelli@gmail.com>, Sijie Guo <sijie@apache.org>
    
    This closes #1813 from merlimat/auto-conf-db-storage-mem

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/conf/TestBKConfiguration.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/conf/TestBKConfiguration.java
@@ -26,6 +26,7 @@ import java.net.SocketException;
 import java.util.Collections;
 import java.util.Enumeration;
 
+import org.apache.bookkeeper.bookie.storage.ldb.DbLedgerStorage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,6 +54,8 @@ public class TestBKConfiguration {
         confReturn.setGcWaitTime(1000);
         confReturn.setDiskUsageThreshold(0.999f);
         confReturn.setDiskUsageWarnThreshold(0.99f);
+        confReturn.setProperty(DbLedgerStorage.WRITE_CACHE_MAX_SIZE_MB, 4);
+        confReturn.setProperty(DbLedgerStorage.READ_AHEAD_CACHE_MAX_SIZE_MB, 4);
         setLoopbackInterfaceAndAllowLoopback(confReturn);
         return confReturn;
     }
