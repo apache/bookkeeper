6b602f77883ceaf926e8246ba622c54d048b55a3
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-03-10 18:32:27 +0800

    Migrate command `bookieinfo` to bkctl
    ### Motivation
    
    Migrate command `bookieinfo` to bkctl.
    
    ### Changes
    
    You can execute as `bin/bkctl bookie  info`.
    
    
    Reviewers: Sijie Guo <sijie@apache.org>
    
    This closes #1972 from zymap/yongzhang/command_info

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/BookieShell.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/BookieShell.java
@@ -38,7 +38,6 @@ import io.netty.util.concurrent.DefaultThreadFactory;
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
-import java.math.RoundingMode;
 import java.net.URI;
 import java.nio.ByteBuffer;
 import java.nio.file.FileSystems;
@@ -47,7 +46,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
@@ -87,7 +85,6 @@ import org.apache.bookkeeper.client.BKException.MetaStoreException;
 import org.apache.bookkeeper.client.BookKeeper;
 import org.apache.bookkeeper.client.BookKeeper.DigestType;
 import org.apache.bookkeeper.client.BookKeeperAdmin;
-import org.apache.bookkeeper.client.BookieInfoReader.BookieInfo;
 import org.apache.bookkeeper.client.LedgerEntry;
 import org.apache.bookkeeper.client.LedgerHandle;
 import org.apache.bookkeeper.client.UpdateLedgerOp;
@@ -113,6 +110,7 @@ import org.apache.bookkeeper.replication.ReplicationException.CompatibilityExcep
 import org.apache.bookkeeper.replication.ReplicationException.UnavailableException;
 import org.apache.bookkeeper.stats.NullStatsLogger;
 import org.apache.bookkeeper.tools.cli.commands.bookie.LastMarkCommand;
+import org.apache.bookkeeper.tools.cli.commands.bookies.InfoCommand;
 import org.apache.bookkeeper.tools.cli.commands.bookies.ListBookiesCommand;
 import org.apache.bookkeeper.tools.cli.commands.client.SimpleTestCommand;
 import org.apache.bookkeeper.tools.cli.commands.cookie.CreateCookieCommand;
@@ -2538,46 +2536,10 @@ public class BookieShell implements Tool {
             return lOpts;
         }
 
-        String getReadable(long val) {
-            String unit[] = {"", "KB", "MB", "GB", "TB"};
-            int cnt = 0;
-            double d = val;
-            while (d >= 1000 && cnt < unit.length - 1) {
-                d = d / 1000;
-                cnt++;
-            }
-            DecimalFormat df = new DecimalFormat("#.###");
-            df.setRoundingMode(RoundingMode.DOWN);
-            return cnt > 0 ? "(" + df.format(d) + unit[cnt] + ")" : unit[cnt];
-        }
-
         @Override
         public int runCmd(CommandLine cmdLine) throws Exception {
-            ClientConfiguration clientConf = new ClientConfiguration(bkConf);
-            clientConf.setDiskWeightBasedPlacementEnabled(true);
-            BookKeeper bk = new BookKeeper(clientConf);
-
-            Map<BookieSocketAddress, BookieInfo> map = bk.getBookieInfo();
-            if (map.size() == 0) {
-                System.out.println("Failed to retrieve bookie information from any of the bookies");
-                bk.close();
-                return 0;
-            }
-
-            System.out.println("Free disk space info:");
-            long totalFree = 0, total = 0;
-            for (Map.Entry<BookieSocketAddress, BookieInfo> e : map.entrySet()) {
-                BookieInfo bInfo = e.getValue();
-                BookieSocketAddress bookieId = e.getKey();
-                System.out.println(getBookieSocketAddrStringRepresentation(bookieId) + ":\tFree: "
-                        + bInfo.getFreeDiskSpace() + getReadable(bInfo.getFreeDiskSpace()) + "\tTotal: "
-                        + bInfo.getTotalDiskSpace() + getReadable(bInfo.getTotalDiskSpace()));
-                totalFree += bInfo.getFreeDiskSpace();
-                total += bInfo.getTotalDiskSpace();
-            }
-            System.out.println("Total free disk space in the cluster:\t" + totalFree + getReadable(totalFree));
-            System.out.println("Total disk capacity in the cluster:\t" + total + getReadable(total));
-            bk.close();
+            InfoCommand cmd = new InfoCommand();
+            cmd.apply(bkConf, new CliFlags());
             return 0;
         }
     }
