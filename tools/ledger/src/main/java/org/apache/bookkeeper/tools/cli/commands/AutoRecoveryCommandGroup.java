5a4b496ec791ac7ddd190cfbe716ffb201748b2d
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-04-17 10:19:25 +0800

    Fix conflict ---
    (If this PR fixes a github issue, please add `Fixes #<xyz>`)
    
    Fixes #<xyz>
    
    (or if this PR is one task of a github issue, please add `Master Issue: #<xyz>`
    to link to the master issue)
    
    Master Issue: #<xyz>
    
    *Motivation*
    
    Describe here the context, and why you're making that change.
    What is the problem you're trying to solve.
    
    *Modifications*
    
    Describe the modifications you've done.
    
    *Verify this change*
    
    Please pick either of following options.
    
    - This change is a trivial rework / code cleanup without any test coverage.
    - This change is already covered by existing tests, such as *(please describe tests)*.
    - This change added tests and can be verified as follows:
      *(example:)*
      - *Added integration tests for end-to-end deployment*
      - *Extended integration test for recovery after broker failure*

--- tools/ledger/src/main/java/org/apache/bookkeeper/tools/cli/commands/AutoRecoveryCommandGroup.java
+++ tools/ledger/src/main/java/org/apache/bookkeeper/tools/cli/commands/AutoRecoveryCommandGroup.java
@@ -20,6 +20,8 @@ package org.apache.bookkeeper.tools.cli.commands;
 
 import static org.apache.bookkeeper.tools.common.BKCommandCategories.CATEGORY_INFRA_SERVICE;
 
+import org.apache.bookkeeper.tools.cli.commands.autorecovery.ListUnderReplicatedCommand;
+import org.apache.bookkeeper.tools.cli.commands.autorecovery.LostBookieRecoveryDelayCommand;
 import org.apache.bookkeeper.tools.cli.commands.autorecovery.ToggleCommand;
 import org.apache.bookkeeper.tools.cli.commands.autorecovery.TriggerAuditCommand;
 import org.apache.bookkeeper.tools.cli.commands.autorecovery.WhoIsAuditorCommand;
@@ -42,6 +44,9 @@ public class AutoRecoveryCommandGroup extends CliCommandGroup<BKFlags> {
         .addCommand(new WhoIsAuditorCommand())
         .addCommand(new TriggerAuditCommand())
         .addCommand(new ToggleCommand())
+        .addCommand(new TriggerAuditCommand())
+        .addCommand(new ListUnderReplicatedCommand())
+        .addCommand(new LostBookieRecoveryDelayCommand())
         .build();
 
     public AutoRecoveryCommandGroup() {
