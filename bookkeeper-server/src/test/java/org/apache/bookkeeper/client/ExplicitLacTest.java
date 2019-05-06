36c1b940711c20388d622dfa3492745cf555b1d3
Yong Zhang <zhangyong1025.zy@gmail.com>
2019-05-06 18:14:32 +0800

    Fix pr validation

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/ExplicitLacTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/ExplicitLacTest.java
@@ -23,7 +23,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Enumeration;
