4cf41d57527c3dc11ab6d9ff237cf7f1d33cb506
Enrico Olivelli <eolivelli@apache.org>
2019-10-30 12:23:47 +0000

    Add missing -Darguments

--- dev/release/001-release-branch.sh
+++ dev/release/001-release-branch.sh
@@ -25,5 +25,6 @@ cd $BK_HOME
 mvn release:branch \
     -DbranchName=${BRANCH_NAME} \
     -DdevelopmentVersion=${DEVELOPMENT_VERSION}
+    -Darguments="-Dmaven.javadoc.skip=true -DskipTests=true -Dstream -Dtwitter" \
     -Dstream
     -Dtwitter
