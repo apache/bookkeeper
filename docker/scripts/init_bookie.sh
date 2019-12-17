1bb85e7eff03b6fdf903c8a049d354fa5916994b
Ravi Sharda <ravi.sharda@emc.com>
2019-12-16 23:38:12 -0800

    Save intermediate changes.
    Signed-off-by: Ravi Sharda <ravi.sharda@emc.com>

--- docker/scripts/init_bookie.sh
+++ docker/scripts/init_bookie.sh
@@ -19,18 +19,40 @@
 # * See the License for the specific language governing permissions and
 # * limitations under the License.
 # */
-
+set -x
 source ${SCRIPTS_DIR}/common.sh
 
 function wait_for_zookeeper() {
     echo "wait for zookeeper"
-    until /opt/bookkeeper/bin/bookkeeper org.apache.zookeeper.ZooKeeperMain -server ${BK_zkServers} ls /; do sleep 5; done
+    until zk-shell --run-once "ls /" ${BK_zkServers}; do sleep 5; done
 }
 
 function create_zk_root() {
     if [ "x${BK_CLUSTER_ROOT_PATH}" != "x" ]; then
         echo "create the zk root dir for bookkeeper at '${BK_CLUSTER_ROOT_PATH}'"
-        /opt/bookkeeper/bin/bookkeeper org.apache.zookeeper.ZooKeeperMain -server ${BK_zkServers} create ${BK_CLUSTER_ROOT_PATH}
+        zk-shell --run-once "create ${BK_CLUSTER_ROOT_PATH} '' false false true" ${BK_zkServers}
+    fi
+}
+
+function create_zk_stream_storage_root() {
+    if [ "x${BK_STREAM_STORAGE_ROOT_PATH}" != "x" ]; then
+        echo "create the zk stream storage dir for bookkeeper at '${BK_STREAM_STORAGE_ROOT_PATH}'"
+        zk-shell --run-once "create ${BK_STREAM_STORAGE_ROOT_PATH} '' false false true" ${BK_zkServers}
+    fi
+}
+
+format_bookie_data_and_metadata() {
+    if [ `find $BK_journalDirectory $BK_ledgerDirectories $BK_indexDirectories -type f 2> /dev/null | wc -l` -gt 0 ]; then
+      # The container already contains data in BK directories. This is probably because
+      # the container has been restarted; or, if running on Kubernetes, it has probably been
+      # updated or evacuated without losing its persistent volumes.
+      echo "Data available in bookkeeper directories; not formatting the bookie"
+    else
+      # The container does not contain any BK data, it is probably a new
+      # bookie. We will format any pre-existent data and metadata before starting
+      # the bookie to avoid potential conflicts.
+      echo "Formatting bookie data and metadata"
+      /opt/bookkeeper/bin/bookkeeper shell bookieformat -nonInteractive -force -deleteCookie || true
     fi
 }
 
@@ -42,16 +64,19 @@ function init_cluster() {
         exit -1
     fi
 
-    /opt/bookkeeper/bin/bookkeeper org.apache.zookeeper.ZooKeeperMain -server ${BK_zkServers} stat ${BK_STREAM_STORAGE_ROOT_PATH}
+    # /opt/bookkeeper/bin/bookkeeper org.apache.zookeeper.ZooKeeperMain -server ${BK_zkServers} stat ${BK_STREAM_STORAGE_ROOT_PATH}
+    zk-shell --run-once "ls ${BK_STREAM_STORAGE_ROOT_PATH}" ${BK_zkServers}
     if [ $? -eq 0 ]; then
         echo "Metadata of cluster already exists, no need to init"
     else
         # create ephemeral zk node bkInitLock, initiator who this node, then do init; other initiators will wait.
-        /opt/bookkeeper/bin/bookkeeper org.apache.zookeeper.ZooKeeperMain -server ${BK_zkServers} create -e ${BK_CLUSTER_ROOT_PATH}/bkInitLock
-        if [ $? -eq 0 ]; then
+        # /opt/bookkeeper/bin/bookkeeper org.apache.zookeeper.ZooKeeperMain -server ${BK_zkServers} create -e ${BK_CLUSTER_ROOT_PATH}/bkInitLock
+        lock=`zk-shell --run-once "create ${BK_CLUSTER_ROOT_PATH}/bkInitLock '' true false false" ${BK_zkServers}`
+        if [ -z "$lock" ]; then
             # bkInitLock created success, this is the successor to do znode init
             echo "Initializing bookkeeper cluster at service uri ${BK_metadataServiceUri}."
-            /opt/bookkeeper/bin/bkctl --service-uri ${BK_metadataServiceUri} cluster init
+            #/opt/bookkeeper/bin/bkctl --service-uri ${BK_metadataServiceUri} cluster init
+            /opt/bookkeeper/bin/bookkeeper shell initnewcluster
             if [ $? -eq 0 ]; then
                 echo "Successfully initialized bookkeeper cluster at service uri ${BK_metadataServiceUri}."
             else
@@ -65,7 +90,8 @@ function init_cluster() {
             do
                 sleep 10
                 echo "run '/opt/bookkeeper/bin/bookkeeper org.apache.zookeeper.ZooKeeperMain -server ${BK_zkServers} stat ${BK_STREAM_STORAGE_ROOT_PATH}'"
-                /opt/bookkeeper/bin/bookkeeper org.apache.zookeeper.ZooKeeperMain -server ${BK_zkServers} stat ${BK_STREAM_STORAGE_ROOT_PATH}
+                # /opt/bookkeeper/bin/bookkeeper org.apache.zookeeper.ZooKeeperMain -server ${BK_zkServers} stat ${BK_STREAM_STORAGE_ROOT_PATH}
+                zk-shell --run-once "ls ${BK_STREAM_STORAGE_ROOT_PATH}" ${BK_zkServers}
                 if [ $? -eq 0 ]; then
                     echo "Waited $tenSeconds * 10 seconds, bookkeeper inited"
                     break
@@ -95,7 +121,9 @@ function init_bookie() {
     # create zookeeper root
     create_zk_root
 
+    # create zookeeper stream storage root
+    # create_zk_stream_storage_root
+
     # init the cluster
     init_cluster
-
 }
