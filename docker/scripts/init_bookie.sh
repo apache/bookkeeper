9b3382d6cfbb68f1ef518d5e6879b9d479390f68
Ravi Sharda <ravi.sharda@emc.com>
2019-12-17 01:31:16 -0800

    Remove debugging flag
    Signed-off-by: Ravi Sharda <ravi.sharda@emc.com>

--- docker/scripts/init_bookie.sh
+++ docker/scripts/init_bookie.sh
@@ -19,7 +19,6 @@
 # * See the License for the specific language governing permissions and
 # * limitations under the License.
 # */
-set -x
 source ${SCRIPTS_DIR}/common.sh
 
 function wait_for_zookeeper() {
