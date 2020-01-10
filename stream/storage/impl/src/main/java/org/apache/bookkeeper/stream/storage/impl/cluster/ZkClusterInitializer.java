558e6386891557d649370624ec450efe6cbdc62b
Sun Dapeng <sundapeng@cmss.chinamobile.com>
2020-01-10 13:37:20 +0800

    Increase the segment store path in the stream cluster log

--- stream/storage/impl/src/main/java/org/apache/bookkeeper/stream/storage/impl/cluster/ZkClusterInitializer.java
+++ stream/storage/impl/src/main/java/org/apache/bookkeeper/stream/storage/impl/cluster/ZkClusterInitializer.java
@@ -32,6 +32,7 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.KeeperException;
+import static org.apache.bookkeeper.stream.storage.StorageConstants.getSegmentsRootPath;
 
 /**
  * ZooKeeper Based Cluster Initializer.
@@ -73,8 +74,6 @@ public class ZkClusterInitializer implements ClusterInitializer  {
                 return false;
             } catch (StorageRuntimeException sre) {
                 if (sre.getCause() instanceof KeeperException.NoNodeException) {
-                    log.info("Initializing the stream cluster with {} storage containers with segment store path {}.",
-                        numStorageContainers);
 
                     String ledgersPath = metadataServiceUri.getPath();
                     Optional<String> segmentStorePath;
@@ -83,6 +82,9 @@ public class ZkClusterInitializer implements ClusterInitializer  {
                     } else {
                         segmentStorePath = Optional.of(ledgersPath);
                     }
+
+                    log.info("Initializing the stream cluster with {} storage containers with segment store path {}.",
+                            numStorageContainers, segmentStorePath.orElse(getSegmentsRootPath(ZK_METADATA_ROOT_PATH)));
 
                     boolean initialized = store.initializeCluster(numStorageContainers, segmentStorePath);
                     log.info("Successfully initialized the stream cluster : \n{}", store.getClusterMetadata());
