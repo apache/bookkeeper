d72abb544e481742c95bbecf20fe297225d7784b
Enrico Olivelli <eolivelli@gmail.com>
2020-03-07 14:46:45 +0100

    Drop Maven Profile -Dstream (#2233)
    ### Motivation
    
    The "stream" profile enables the build of all of the additional packages regarding the 'table service'.
    This profile complicates a lot every script and the Maven project.
    As the stream storage service is now stable and used in production, it is better to always include those modules.
    Regualar developers on bookkeeper-server module won't be affected by this change as it affects only the full build and the release process.
    
    ### Changes
    Modify every pom.xml in order to enable all of the parts of the pom.xml that would be enabled with '-Dstream'.
    Modify release scripts by removing references to -Dstream
    Modify bin/common.sh about the auto build feature
    Modify Groovy scripts for Jenkins
    Do not "shade" libthrift inside Distributed Log Jar
    
    I did not modify the website because currently the precommit validation of the website is broken
    
    
    * Remove stream Maven Profile
    
    * Delete job_bookkeeper_precommit_java11.groovy
    
    * Delete job_bookkeeper_precommit_java8.groovy
    
    * Delete ticktoc.sh
    
    * Restore -DstreamTests profile
    
    Co-authored-by: Enrico Olivelli <eolivelli@apache.org>

--- .test-infra/jenkins/job_bookkeeper_postcommit_master_java11.groovy
+++ .test-infra/jenkins/job_bookkeeper_postcommit_master_java11.groovy
@@ -39,5 +39,5 @@ mavenJob('bookkeeper_postcommit_master_java11') {
   common_job_properties.setMavenConfig(delegate)
 
   // Maven build project.
-  goals('clean package spotbugs:check -Ddistributedlog -Dstream -DstreamTests')
+  goals('clean package spotbugs:check -Ddistributedlog -DstreamTests')
 }
