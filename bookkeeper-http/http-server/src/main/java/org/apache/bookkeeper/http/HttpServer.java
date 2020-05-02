a1b187e338fd5892df7816d48c153e61302483cc
Yang Yang <yyang@streamnative.io>
2020-05-02 15:50:45 +0800

    ISSUE #2310: Adds the autorecovery status service.
    Descriptions of the changes in this PR:
    
    
    
    ### Motivation
    
    It would be convenient to support enable, disable, or check the status of autorecovery with REST API, just like what does in the toggle command.
    
    ### Changes
    
    Adds an API to enable, disable, and get the current status of autorecovery at `/api/v1/autorecovery/status`.
    
    Master Issue: #2310 
    
    
    
    Reviewers: Sijie Guo <None>, Enrico Olivelli <eolivelli@gmail.com>, Matteo Minardi <minardi.matteo@hotmail.it>
    
    This closes #2313 from fantapsody/autorecovery_status, closes #2310

--- bookkeeper-http/http-server/src/main/java/org/apache/bookkeeper/http/HttpServer.java
+++ bookkeeper-http/http-server/src/main/java/org/apache/bookkeeper/http/HttpServer.java
@@ -33,6 +33,7 @@ public interface HttpServer {
     enum StatusCode {
         OK(200),
         REDIRECT(302),
+        BAD_REQUEST(400),
         FORBIDDEN(403),
         NOT_FOUND(404),
         INTERNAL_ERROR(500),
@@ -84,6 +85,7 @@ public interface HttpServer {
         BOOKIE_IS_READY,
 
         // autorecovery
+        AUTORECOVERY_STATUS,
         RECOVERY_BOOKIE,
         LIST_UNDER_REPLICATED_LEDGER,
         WHO_IS_AUDITOR,
