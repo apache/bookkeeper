0bea124989d68376249826e35b179a755eda9025
Charan Reddy Guttapalem <cguttapalem@salesforce.com>
2020-06-12 08:05:39 -0700

    Remove lock from URManager’s state only if lock znode deletion has succeeded
    Descriptions of the changes in this PR:
    
    - in ZkLedgerUnderreplicationManager.releaseUnderreplicatedLedger remove ‘lock’
     from ‘heldLocks’ only if lock znode deletion has succeeded.
    - This is needed because, if RW.logBKExceptionAndReleaseLedger fails to delete
     the lock znode, then it needs to be tried once more in the finally block of
     'rereplicate(long ledgerIdToReplicate)’ before giving up and shutting down RW.
    
    Reviewers: Enrico Olivelli <eolivelli@gmail.com>
    
    This closes #2206 from reddycharan/fixzklossissue

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/meta/ZkLedgerUnderreplicationManager.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/meta/ZkLedgerUnderreplicationManager.java
@@ -629,7 +629,7 @@ public class ZkLedgerUnderreplicationManager implements LedgerUnderreplicationMa
             LOG.debug("releaseLedger(ledgerId={})", ledgerId);
         }
         try {
-            Lock l = heldLocks.remove(ledgerId);
+            Lock l = heldLocks.get(ledgerId);
             if (l != null) {
                 zkc.delete(l.getLockZNode(), -1);
             }
@@ -642,6 +642,7 @@ public class ZkLedgerUnderreplicationManager implements LedgerUnderreplicationMa
             Thread.currentThread().interrupt();
             throw new ReplicationException.UnavailableException("Interrupted while connecting zookeeper", ie);
         }
+        heldLocks.remove(ledgerId);
     }
 
     @Override
