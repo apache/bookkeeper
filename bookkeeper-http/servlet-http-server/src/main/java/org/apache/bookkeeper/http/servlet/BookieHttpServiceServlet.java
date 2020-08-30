ca48cebfe70d32d4b7ad3bdb184285b824043537
rudy2steiner <rudy_steiner@163.com>
2020-08-31 01:12:44 +0800

    checkstyle

--- bookkeeper-http/servlet-http-server/src/main/java/org/apache/bookkeeper/http/servlet/BookieHttpServiceServlet.java
+++ bookkeeper-http/servlet-http-server/src/main/java/org/apache/bookkeeper/http/servlet/BookieHttpServiceServlet.java
@@ -1,3 +1,23 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
 package org.apache.bookkeeper.http.servlet;
 
 import java.io.IOException;
@@ -6,7 +26,6 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,7 +43,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Bookie http service servlet
+ * Bookie http service servlet.
  *
  **/
 public class BookieHttpServiceServlet extends HttpServlet {
@@ -50,7 +69,7 @@ public class BookieHttpServiceServlet extends HttpServlet {
   }
 
   @Override
-  protected void service(HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws ServletException, IOException {
+  protected void service(HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws IOException {
     HttpServiceRequest request = new HttpServiceRequest()
                                 .setMethod(httpServerMethod(httpRequest))
                                 .setParams(httpServletParams(httpRequest))
@@ -71,7 +90,7 @@ public class BookieHttpServiceServlet extends HttpServlet {
       }
       response = httpEndpointService.handle(request);
     } catch (Throwable e) {
-      LOG.error( "Error while service Bookie API request " + uri, e);
+      LOG.error("Error while service Bookie API request " + uri, e);
       response = new ErrorHttpService().handle(request);
     }
     if (response != null) {
@@ -110,8 +129,10 @@ public class BookieHttpServiceServlet extends HttpServlet {
         return HttpServer.Method.DELETE;
       case "PUT":
         return HttpServer.Method.PUT;
-      default:
+      case "GET":
         return HttpServer.Method.GET;
+      default:
+        throw new UnsupportedOperationException("Unsupported http method");
     }
   }
 }
--- bookkeeper-http/servlet-http-server/src/test/java/org/apache/bookkeeper/http/servlet/TestBookieHttpServiceServlet.java
+++ bookkeeper-http/servlet-http-server/src/test/java/org/apache/bookkeeper/http/servlet/TestBookieHttpServiceServlet.java
@@ -1,3 +1,23 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
 package org.apache.bookkeeper.http.servlet;
 
 import static org.hamcrest.CoreMatchers.containsString;
@@ -14,27 +34,30 @@ import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+/**
+ * Test bookie http service servlet.
+ **/
 public class TestBookieHttpServiceServlet {
 
   private JettyHttpServer jettyHttpServer;
-  private String host="localhost";
-  int port=8080;
+  private String host = "localhost";
+  private int port = 8080;
   private BookieServletHttpServer bookieServletHttpServer;
   @Before
   public void setUp() throws Exception {
-    this.bookieServletHttpServer=new BookieServletHttpServer();
+    this.bookieServletHttpServer = new BookieServletHttpServer();
     this.bookieServletHttpServer.initialize(new NullHttpServiceProvider());
-    this.jettyHttpServer=new JettyHttpServer(host,port);
-    List<Servlet> servlets=new ArrayList<>();
+    this.jettyHttpServer = new JettyHttpServer(host, port);
+    List<Servlet> servlets = new ArrayList<>();
     servlets.add(new BookieHttpServiceServlet());
-    jettyHttpServer.addServlet("web/bookie","/","/", servlets);
+    jettyHttpServer.addServlet("web/bookie", "/", "/",  servlets);
     jettyHttpServer.startServer();
   }
 
   @Test
-  public void testBookieHeartBeat() throws URISyntaxException,IOException {
-    assertThat(IOUtils.toString(new URI(String.format("http://%s:%d/heartbeat",host,port)), "UTF-8"), containsString("OK"));
+  public void testBookieHeartBeat() throws URISyntaxException, IOException {
+    assertThat(IOUtils.toString(new URI(String.format("http://%s:%d/heartbeat", host, port)), "UTF-8"),
+                                                                                  containsString("OK"));
   }
 
   @After
