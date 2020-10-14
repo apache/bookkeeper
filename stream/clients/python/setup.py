be922fe5bd6a5300c64f547a918d3236d7c93681
ca-simone-chiorazzo <44833596+ca-simone-chiorazzo@users.noreply.github.com>
2020-10-14 16:35:33 +0200

    ISSUE #2377: Relax grpcio requirement for python client to support python3.8 on mac os
    Descriptions of the changes in this PR:
    
    Require a grpcio version that is minor of 1.28. This allow to install the 1.27.2 that has a pre-compiled wheel for python 3.8 on mac os.
    Add tests for python 3.8
    
    ### Motivation
    
    grpcio 1.25.0 does not allow the installation using python 3.8 on mac os x. This is due to the fact that the pre-compiled wheels have not been published before 1.26.0
    
    ### Changes
    
    Updated the setup.py to relax a bit the requirement for grpcio
    Add python3.8 as tested version
    Updated the setup.py to install the latest pymmh3 version. **Note**: master tests are broken, I've updated the library and removed the 'signed' parameter that does not exists not anymore.
    
    Master Issue: #<master-issue-number>
    
    cc eolivelli
    
    Reviewers: Enrico Olivelli <eolivelli@gmail.com>, Sijie Guo <sijie@apache.org>
    
    This closes #2432 from ca-simone-chiorazzo/bugfix/2377-relax-grpcio-python-dep-for-python-3.8-on-mac-os, closes #2377
    
    (cherry picked from commit d3af27e202716815d2fc1fd8607853c902653788)
    Signed-off-by: Enrico Olivelli <eolivelli@apache.org>

--- stream/clients/python/setup.py
+++ stream/clients/python/setup.py
@@ -32,8 +32,8 @@ dependencies = [
     'six>=1.10.0',
     'pytz',
     'futures>=3.2.0;python_version<"3.2"',
-    'grpcio<1.26.0,>=1.8.2',
-    'pymmh3>=0.0.3'
+    'grpcio<1.28,>=1.8.2',
+    'pymmh3>=0.0.5'
 ]
 extras = {
 }
