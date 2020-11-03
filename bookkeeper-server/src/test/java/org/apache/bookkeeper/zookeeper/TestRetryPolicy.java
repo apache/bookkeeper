bbb0113d61d3c4c47079f5924ad00bc433925b63
karanmehta93 <k.mehta@salesforce.com>
2020-11-03 02:03:44 -0800

    Introduce new retry policy for metadata operations (#2470)
    Descriptions of the changes in this PR:
    
    ### Motivation
    Current retry backoff policy is truly exponential, hence retries are spaced out at large intervals. There is maxBackOff time that limits it, but it doesn't have a concept of deadline. Hence, applications with shorter SLA's have issues dealing with ZK failures and recovering from it.
    
    ### Changes
    
    The new policy introduces an aggressive retry mechanism with the backoff constants as less exponential. The deadline ensures that we retry at least once at the deadline. The stack trace would indicate the operation at risk.
    
    This doesn't update the product code, which is still in testing. I am splitting PR's for for sake of reviews.
    
    
    * Introduce new retry policy for metadata operations
    
    * Spotbugs error fix

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/zookeeper/TestRetryPolicy.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/zookeeper/TestRetryPolicy.java
@@ -20,6 +20,8 @@
  */
 package org.apache.bookkeeper.zookeeper;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -54,5 +56,33 @@ public class TestRetryPolicy {
         assertTimeRange(policy.nextRetryWaitTime(127, 2000), 1000L, 1000L);
         assertTimeRange(policy.nextRetryWaitTime(128, 2000), 1000L, 2000L);
         assertTimeRange(policy.nextRetryWaitTime(129, 2000), 1000L, 2000L);
+    }
+
+    @Test
+    public void testExponentialBackoffWithDeadlineRetryPolicy() throws Exception {
+        RetryPolicy policy = new ExponentialBackOffWithDeadlinePolicy(100, 55 * 1000, 20);
+
+        // Retries are allowed as long as we don't exceed the limits of retry count and deadline
+        assertTrue(policy.allowRetry(1, 5 * 1000));
+        assertTrue(policy.allowRetry(4, 20 * 1000));
+        assertTrue(policy.allowRetry(10, 50 * 1000));
+
+        assertFalse(policy.allowRetry(0, 60 * 1000));
+        assertFalse(policy.allowRetry(22, 20 * 1000));
+        assertFalse(policy.allowRetry(22, 60 * 1000));
+
+        // Verify that the wait times are in the range and with the excepted jitter, until deadline is exceeded
+        assertTimeRange(policy.nextRetryWaitTime(0, 0), 0, 0);
+        assertTimeRange(policy.nextRetryWaitTime(1, 0), 100, 110);
+        assertTimeRange(policy.nextRetryWaitTime(1, 53 * 1000), 100, 110);
+        assertTimeRange(policy.nextRetryWaitTime(2, 0), 200, 220);
+        assertTimeRange(policy.nextRetryWaitTime(3, 0), 300, 330);
+        assertTimeRange(policy.nextRetryWaitTime(3, 53 * 1000), 300, 330);
+        assertTimeRange(policy.nextRetryWaitTime(4, 0), 500, 550);
+        assertTimeRange(policy.nextRetryWaitTime(5, 0), 500, 550);
+
+        // Verify that the final attempt is triggered at deadline.
+        assertEquals(2000, policy.nextRetryWaitTime(10, 53 * 1000));
+        assertEquals(4000, policy.nextRetryWaitTime(15, 51 * 1000));
     }
 }
