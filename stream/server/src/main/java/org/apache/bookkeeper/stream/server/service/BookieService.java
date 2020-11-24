047a6e927b29fcc62c71946c37b14e17166596ad
Enrico Olivelli <eolivelli@gmail.com>
2020-11-24 14:03:17 +0100

    Publish BP-38 BookieEndpointInfo for BookKeeper Standalone mode
    ### Motivation
    
    When you run "bin/bookkeeper standalone" the server does not publish BP-38 endpoint info.
    This way you cannot use bookieId (BP-41).
    
    ### Changes
    
    Fix BookieService in "standalone" package, that is very like to the BookieService in "bookkeeper-server".
    The code cannot be reused, even if the name is similar the overall design of the class hierarchy is different.
    
    
    Reviewers: Jia Zhai <zhaijia@apache.org>
    
    This closes #2484 from eolivelli/fix/standalone-bp38

--- stream/server/src/main/java/org/apache/bookkeeper/stream/server/service/BookieService.java
+++ stream/server/src/main/java/org/apache/bookkeeper/stream/server/service/BookieService.java
@@ -13,18 +13,20 @@
  */
 package org.apache.bookkeeper.stream.server.service;
 
-import java.io.File;
 import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.bookkeeper.common.component.AbstractLifecycleComponent;
+import org.apache.bookkeeper.common.component.ComponentInfoPublisher;
 import org.apache.bookkeeper.conf.ServerConfiguration;
 import org.apache.bookkeeper.discover.BookieServiceInfo;
+import org.apache.bookkeeper.net.BookieSocketAddress;
 import org.apache.bookkeeper.proto.BookieServer;
 import org.apache.bookkeeper.stats.StatsLogger;
 import org.apache.bookkeeper.stream.server.conf.BookieConfiguration;
@@ -40,34 +42,27 @@ public class BookieService extends AbstractLifecycleComponent<BookieConfiguratio
     @Getter
     private final ServerConfiguration serverConf;
     private BookieServer bs;
-    private final Supplier<BookieServiceInfo> bookieServiceInfoProvider;
-
 
     public BookieService(BookieConfiguration conf, StatsLogger statsLogger,
-                         Supplier<BookieServiceInfo> bookieServiceInfoProvider) {
+                         Supplier<BookieServiceInfo> bookieServiceInfoProvider) throws Exception {
         super("bookie-server", conf, statsLogger);
         this.serverConf = new ServerConfiguration();
         this.serverConf.loadConf(conf.getUnderlyingConf());
-        this.bookieServiceInfoProvider = bookieServiceInfoProvider;
+        String hello = String.format(
+            "Hello, I'm your bookie, bookieId is %1$s, listening on port %2$s. Metadata service uri is %3$s."
+                + " Journals are in %4$s. Ledgers are stored in %5$s.",
+            serverConf.getBookieId() != null ? serverConf.getBookieId() : "<not-set>",
+            serverConf.getBookiePort(),
+            serverConf.getMetadataServiceUriUnchecked(),
+            Arrays.asList(serverConf.getJournalDirNames()),
+            Arrays.asList(serverConf.getLedgerDirNames()));
+        log.info(hello);
+        this.bs = new BookieServer(serverConf, statsLogger, bookieServiceInfoProvider);
     }
 
     @Override
     protected void doStart() {
-        List<File> indexDirs;
-        if (null == serverConf.getIndexDirs()) {
-            indexDirs = Collections.emptyList();
-        } else {
-            indexDirs = Arrays.asList(serverConf.getIndexDirs());
-        }
-        log.info("Hello, I'm your bookie, listening on port {} :"
-                + " metadata service uri = {}, journals = {}, ledgers = {}, index = {}",
-            serverConf.getBookiePort(),
-            serverConf.getMetadataServiceUriUnchecked(),
-            Arrays.asList(serverConf.getJournalDirNames()),
-            Arrays.asList(serverConf.getLedgerDirs()),
-            indexDirs);
         try {
-            this.bs = new BookieServer(serverConf, statsLogger, bookieServiceInfoProvider);
             bs.start();
             log.info("Started bookie server successfully.");
         } catch (Exception e) {
@@ -85,5 +80,24 @@ public class BookieService extends AbstractLifecycleComponent<BookieConfiguratio
     @Override
     protected void doClose() throws IOException {
         // no-op
+    }
+
+    @Override
+    public void publishInfo(ComponentInfoPublisher componentInfoPublisher) {
+        try {
+            BookieSocketAddress localAddress = bs.getLocalAddress();
+            List<String> extensions = new ArrayList<>();
+            if (serverConf.getTLSProviderFactoryClass() != null) {
+                extensions.add("tls");
+            }
+            ComponentInfoPublisher.EndpointInfo endpoint = new ComponentInfoPublisher.EndpointInfo("bookie",
+                    localAddress.getPort(),
+                    localAddress.getHostName(),
+                    "bookie-rpc", null, extensions);
+            componentInfoPublisher.publishEndpoint(endpoint);
+
+        } catch (UnknownHostException err) {
+            log.error("Cannot compute local address", err);
+        }
     }
 }
