eae122bf8e3f433e9b888fe0f18616569a013dca
Andrey Yegorov <andrey.yegorov@datastax.com>
2021-01-26 09:42:21 -0800

    Fixed failing tests for /stream.
    Tests were failing with " “java.lang.UnsatisfiedLinkError: 'long org.rocksdb.LRUCache.newLRUCache(long, int, boolean, double)'”".
    Fixed following suggestion form https://github.com/facebook/rocksdb/issues/6789

--- stream/statelib/src/main/java/org/apache/bookkeeper/statelib/impl/kv/RocksdbKVStore.java
+++ stream/statelib/src/main/java/org/apache/bookkeeper/statelib/impl/kv/RocksdbKVStore.java
@@ -127,6 +127,10 @@ public class RocksdbKVStore<K, V> implements KVStore<K, V> {
     // rocksdb checkpointer
     private RocksCheckpointer checkpointer;
 
+    static {
+        RocksDB.loadLibrary();
+    }
+
     public RocksdbKVStore() {
         // initialize the iterators set
         this.kvIters = Collections.synchronizedSet(Sets.newHashSet());
