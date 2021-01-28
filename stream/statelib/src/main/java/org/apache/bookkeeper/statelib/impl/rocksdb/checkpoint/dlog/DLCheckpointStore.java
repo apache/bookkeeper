cdec1bbb37e225248a6bb1603f35cc66943546d4
Andrey Yegorov <dlg99@users.noreply.github.com>
2021-01-28 00:12:05 -0800

    Fixed: DLCheckpointStore rethrows exceptions as FNFE but swallows the rootcause
    DLCheckpointStore rethrows exceptions as FNFE but swallows the rootcause
    Same as https://github.com/apache/bookkeeper/pull/2553 but for master branch
    
    Descriptions of the changes in this PR:
    
    added .initCause for the new FNFE exception to preserver it
    
    ### Motivation
    
    got the
    ERROR org.apache.bookkeeper.statelib.impl.rocksdb.checkpoint.RocksCheckpointer - Failed to restore rocksdb ...
    java.io.FileNotFoundException: ..../metadata
    without any additional info for the rootcause (other than stack pointing to the DLCheckpointStore)
    
    DLCheckpointStore appears to re-throw exceptions as FNFE without preserving the rootcause
    
    ### Changes
    
    added .initCause
    
    Master Issue: #2552
    
    Reviewers: Enrico Olivelli <eolivelli@gmail.com>
    
    This closes #2555 from dlg99/master-fnfe
    
    (cherry picked from commit bab5a003e84601469485cbcee4fc784171a1513d)
    Signed-off-by: Enrico Olivelli <eolivelli@apache.org>

--- stream/statelib/src/main/java/org/apache/bookkeeper/statelib/impl/rocksdb/checkpoint/dlog/DLCheckpointStore.java
+++ stream/statelib/src/main/java/org/apache/bookkeeper/statelib/impl/rocksdb/checkpoint/dlog/DLCheckpointStore.java
@@ -55,6 +55,12 @@ public class DLCheckpointStore implements CheckpointStore {
         this.namespace = namespace;
     }
 
+    private static FileNotFoundException createFileNotFoundException(String filePath, Throwable t) {
+        FileNotFoundException fnfe = new FileNotFoundException(filePath);
+        fnfe.initCause(t);
+        return fnfe;
+    }
+
     @Override
     public List<String> listFiles(String filePath) throws IOException {
         return Lists.newArrayList(namespace.getLogs(filePath));
@@ -70,7 +76,7 @@ public class DLCheckpointStore implements CheckpointStore {
         try (DistributedLogManager dlm = namespace.openLog(filePath)) {
             return dlm.getLastTxId();
         } catch (LogNotFoundException e) {
-            throw new FileNotFoundException(filePath);
+            throw createFileNotFoundException(filePath, e);
         } catch (LogEmptyException e) {
             return 0;
         }
@@ -84,12 +90,12 @@ public class DLCheckpointStore implements CheckpointStore {
             try {
                 reader = dlm.openLogReader(DLSN.InitialDLSN);
             } catch (LogNotFoundException | LogEmptyException e) {
-                throw new FileNotFoundException(filePath);
+                throw createFileNotFoundException(filePath, e);
             }
             return new BufferedInputStream(
                 new DLInputStream(dlm, reader, 0L), 128 * 1024);
         } catch (LogNotFoundException e) {
-            throw new FileNotFoundException(filePath);
+            throw createFileNotFoundException(filePath, e);
         }
     }
 
@@ -102,7 +108,7 @@ public class DLCheckpointStore implements CheckpointStore {
             return new BufferedOutputStream(
                 new DLOutputStream(dlm, writer), 128 * 1024);
         } catch (LogNotFoundException le) {
-            throw new FileNotFoundException(filePath);
+            throw createFileNotFoundException(filePath, le);
         }
     }
 
