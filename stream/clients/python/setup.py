ed3d8401bbadb8f277c046ddf19d5e5f13bdad48
Yong Zhang <zhangyong1025.zy@gmail.com>
2021-07-15 09:33:17 +0800

    Remove the `request` in the stream python client dependency ---
    Master Issue: #2752
    
    *Motivation*
    
    As discussed at length in https://issues.apache.org/jira/browse/LEGAL-572
    we found out that the chardet library used by requests library was a
    mandatory dependency to requests and since it has LGPL licence, we
    should not release any Apache Software with it.

--- stream/clients/python/setup.py
+++ stream/clients/python/setup.py
@@ -27,7 +27,6 @@ version = '4.12.0'
 release_status = 'Development Status :: 3 - Alpha'
 dependencies = [
     'protobuf>=3.0.0',
-    'requests<3.0.0dev,>=2.18.0',
     'setuptools>=34.0.0',
     'six>=1.10.0',
     'pytz',
