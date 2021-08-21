1139c12ec12eb19c7490a830ce5787007dff70c2
Yang Yang <yyang@streamnative.io>
2021-08-21 15:48:28 +0800

    Support specifying bookie http port as a command argument

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/server/Main.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/server/Main.java
@@ -76,6 +76,7 @@ public class Main {
         BK_OPTS.addOption("z", "zkserver", true, "Zookeeper Server");
         BK_OPTS.addOption("m", "zkledgerpath", true, "Zookeeper ledgers root path");
         BK_OPTS.addOption("p", "bookieport", true, "bookie port exported");
+        BK_OPTS.addOption("httpport", true, "bookie http port exported");
         BK_OPTS.addOption("j", "journal", true, "bookie journal directory");
         Option indexDirs = new Option ("i", "indexdirs", true, "bookie index directories");
         indexDirs.setArgs(10);
@@ -171,6 +172,13 @@ public class Main {
                 log.info("Get cmdline bookie port: {}", sPort);
                 Integer iPort = Integer.parseInt(sPort);
                 conf.setBookiePort(iPort.intValue());
+            }
+
+            if (cmdLine.hasOption("httpport")) {
+                String sPort = cmdLine.getOptionValue("httpport");
+                log.info("Get cmdline http port: {}", sPort);
+                Integer iPort = Integer.parseInt(sPort);
+                conf.setHttpServerPort(iPort.intValue());
             }
 
             if (cmdLine.hasOption('j')) {
