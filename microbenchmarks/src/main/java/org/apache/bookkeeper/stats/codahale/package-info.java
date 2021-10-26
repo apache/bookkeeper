26a31ee658b9e71574dadc5f64121bcede0a68ec
Nicolò Boschi <boschi1997@gmail.com>
2021-10-26 12:24:34 +0200

    reformat

--- microbenchmarks/src/main/java/org/apache/bookkeeper/stats/codahale/package-info.java
+++ microbenchmarks/src/main/java/org/apache/bookkeeper/stats/codahale/package-info.java
@@ -15,9 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * Stats implementations benchmarks.
- */
+
 /**
  * Stats benchmark runner.
  */
