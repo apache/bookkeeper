395198fc9fe739e0a42366c6a6edf7641e72596f
lushiji <lushiji@didiglobal.com>
2021-11-24 17:59:47 +0800

    auto metaformat

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/BookieShell.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/BookieShell.java
@@ -2328,7 +2328,7 @@ public class BookieShell implements Tool {
         }
     }
 
-    public static void main(String[] argv) throws Exception {
+    public static int doMain(String[] argv) throws Exception {
         BookieShell shell = new BookieShell();
 
         // handle some common options for multiple cmds
@@ -2366,7 +2366,11 @@ public class BookieShell implements Tool {
         }
         LOG.debug("Using entry formatter {}", shell.entryFormatter.getClass());
 
-        int res = shell.run(cmdLine.getArgs());
+        return shell.run(cmdLine.getArgs());
+    }
+
+    public static void main(String[] argv) throws Exception {
+        int res = doMain(argv);
         System.exit(res);
     }
 
--- bookkeeper-server/src/main/java/org/apache/bookkeeper/server/Main.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/server/Main.java
@@ -29,6 +29,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.bookkeeper.bookie.BookieShell;
 import org.apache.bookkeeper.bookie.ExitCode;
 import org.apache.bookkeeper.bookie.ScrubberStats;
 import org.apache.bookkeeper.common.component.ComponentInfoPublisher;
@@ -39,6 +40,7 @@ import org.apache.bookkeeper.conf.ServerConfiguration;
 import org.apache.bookkeeper.conf.UncheckedConfigurationException;
 import org.apache.bookkeeper.discover.BookieServiceInfo;
 import org.apache.bookkeeper.discover.BookieServiceInfo.Endpoint;
+import org.apache.bookkeeper.meta.zk.ZKMetadataDriverBase;
 import org.apache.bookkeeper.server.component.ServerLifecycleComponent;
 import org.apache.bookkeeper.server.conf.BookieConfiguration;
 import org.apache.bookkeeper.server.http.BKHttpServiceProvider;
@@ -67,6 +69,7 @@ import org.apache.commons.lang3.StringUtils;
 public class Main {
 
     static final Options BK_OPTS = new Options();
+    static String CONF_FILE = null;
     static {
         BK_OPTS.addOption("c", "conf", true, "Configuration for Bookie Server");
         BK_OPTS.addOption("withAutoRecovery", false,
@@ -220,16 +223,25 @@ public class Main {
             return ExitCode.INVALID_CONF;
         }
 
-        // 1. building the component stack:
+        BookieConfiguration bkConf = new BookieConfiguration(conf);
+        // 1. find no node in zk，run command: "bin/bookkeeper shell metaformat"
+        try {
+            autoMetaFormat(conf, bkConf);
+        } catch (Exception e) {
+            log.error("Failed to metaformat", e);
+            return ExitCode.SERVER_EXCEPTION;
+        }
+
+        // 2. building the component stack:
         LifecycleComponent server;
         try {
-            server = buildBookieServer(new BookieConfiguration(conf));
+            server = buildBookieServer(bkConf);
         } catch (Exception e) {
             log.error("Failed to build bookie server", e);
             return ExitCode.SERVER_EXCEPTION;
         }
 
-        // 2. start the server
+        // 3. start the server
         try {
             ComponentStarter.startComponent(server).get();
         } catch (InterruptedException ie) {
@@ -267,6 +279,43 @@ public class Main {
         return conf;
     }
 
+    private static void autoMetaFormat(ServerConfiguration conf, BookieConfiguration bkConf) throws Exception {
+        int INSTANCEID_TIMEOUT = 60 * 1000;
+        if (ZKMetadataDriverBase.needMetaformat(bkConf.getServerConf(), conf.getZkRetryBackoffStartMs(), conf.getZkRetryBackoffMaxMs())) {
+            ZKMetadataDriverBase.CreateLockStatus lock = ZKMetadataDriverBase.tryCreateInstanceLock(bkConf.getServerConf(), conf.getZkRetryBackoffStartMs(), conf.getZkRetryBackoffMaxMs());
+            if (lock == ZKMetadataDriverBase.CreateLockStatus.created) {
+                doMetaFormat(conf, bkConf);
+            } else if (lock == ZKMetadataDriverBase.CreateLockStatus.existed) {
+                long start = System.currentTimeMillis();
+                long timeCost = System.currentTimeMillis() - start;
+                while (timeCost < INSTANCEID_TIMEOUT) {
+                    if (!ZKMetadataDriverBase.needMetaformat(bkConf.getServerConf(), conf.getZkRetryBackoffStartMs(), conf.getZkRetryBackoffMaxMs())) {
+                        log.info("metaformat is just now created by another bookie,skip it");
+                        return;
+                    }
+                    lock = ZKMetadataDriverBase.tryCreateInstanceLock(bkConf.getServerConf(), conf.getZkRetryBackoffStartMs(), conf.getZkRetryBackoffMaxMs());
+                    if (lock == ZKMetadataDriverBase.CreateLockStatus.created && doMetaFormat(conf, bkConf)) {
+                        return;
+                    }
+                    Thread.sleep(50);
+                    timeCost = System.currentTimeMillis() - start;
+                }
+            }
+        }
+    }
+
+    private static boolean doMetaFormat(ServerConfiguration conf, BookieConfiguration bkConf) throws Exception {
+        try {
+            int res = BookieShell.doMain(new String[]{"-conf", CONF_FILE, "metaformat","-nonInteractive","-force"});
+            if (res != 0) {
+                log.error("Failed to run metaformat,res={}", res);
+                throw new RuntimeException("Failed to run metaformat,res=" + res);
+            }
+        } finally {
+            ZKMetadataDriverBase.tryCleanInstanceLock(bkConf.getServerConf(), conf.getZkRetryBackoffStartMs(), conf.getZkRetryBackoffMaxMs());
+        }
+        return true;
+    }
     /**
      * Build the bookie server.
      *
