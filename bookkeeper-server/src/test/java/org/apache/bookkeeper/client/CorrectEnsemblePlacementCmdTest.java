4ab4f85cd2d8a12e0c81b1d7d8dd5ec63d54a000
Yuri Mizushima <yumizush@yahoo-corp.jp>
2021-12-07 14:07:02 +0900

    test: add test for relocate placement feature

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/CorrectEnsemblePlacementCmdTest.java
@@ -0,0 +1,235 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.bookkeeper.client;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.apache.bookkeeper.client.RackawareEnsemblePlacementPolicyImpl.REPP_DNS_RESOLVER_CLASS;
+import static org.mockito.ArgumentMatchers.eq;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.Cleanup;
+import org.apache.bookkeeper.bookie.BookieShell;
+import org.apache.bookkeeper.bookie.storage.ldb.DbLedgerStorage;
+import org.apache.bookkeeper.client.api.LedgerMetadata;
+import org.apache.bookkeeper.net.BookieId;
+import org.apache.bookkeeper.net.BookieSocketAddress;
+import org.apache.bookkeeper.net.NetworkTopology;
+import org.apache.bookkeeper.proto.BookieAddressResolver;
+import org.apache.bookkeeper.test.BookKeeperClusterTestCase;
+import org.apache.bookkeeper.tools.cli.commands.bookies.CorrectEnsemblePlacementCommand;
+import org.apache.bookkeeper.util.EntryFormatter;
+import org.apache.bookkeeper.util.LedgerIdFormatter;
+import org.apache.bookkeeper.util.StaticDNSResolver;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Tests of correct-ensemble-placement command.
+ */
+public class CorrectEnsemblePlacementCmdTest extends BookKeeperClusterTestCase {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CorrectEnsemblePlacementCmdTest.class);
+    private BookKeeper.DigestType digestType = BookKeeper.DigestType.CRC32;
+    private static final String PASSWORD = "testPasswd";
+
+    public CorrectEnsemblePlacementCmdTest() throws Exception {
+        super(0);
+        baseConf.setLedgerStorageClass(DbLedgerStorage.class.getName());
+        baseConf.setGcWaitTime(60000);
+        baseConf.setFlushInterval(1);
+    }
+
+    /**
+     * list of entry logger files that contains given ledgerId.
+     */
+    @Test
+    public void testArgument() throws Exception {
+        startNewBookie();
+
+        @Cleanup
+        final BookKeeper bk = new BookKeeper(baseClientConf, zkc);
+        @Cleanup
+        final LedgerHandle lh = createLedgerWithEntries(bk, 10, 1, 1);
+
+        final String[] argv1 = { "correct-ensemble-placement", "--ledgerids", String.valueOf(lh.getId()),
+                "--skipOpenLedgers", "--force"};
+        final String[] argv2 = { "correct-ensemble-placement", "--ledgerids", String.valueOf(lh.getId()),
+                "--skipOpenLedgers", "--force", "--dryrun"};
+        final BookieShell bkShell =
+                new BookieShell(LedgerIdFormatter.LONG_LEDGERID_FORMATTER, EntryFormatter.STRING_FORMATTER);
+        bkShell.setConf(baseClientConf);
+
+        assertEquals("Failed to return exit code!", 0, bkShell.run(argv1));
+        assertEquals("Failed to return exit code!", 0, bkShell.run(argv2));
+    }
+
+    @Test
+    public void testCorrectEnsemblePlacementByRackaware() throws Exception {
+        final int ensembleSize = 7;
+        final int quorumSize = 2;
+
+        final BookieSocketAddress addr1 = new BookieSocketAddress("127.0.0.1", 3181);
+        final BookieSocketAddress addr2 = new BookieSocketAddress("127.0.0.2", 3181);
+        final BookieSocketAddress addr3 = new BookieSocketAddress("127.0.0.3", 3181);
+        final BookieSocketAddress addr4 = new BookieSocketAddress("127.0.0.4", 3181);
+        final BookieSocketAddress addr5 = new BookieSocketAddress("127.0.0.5", 3181);
+        final BookieSocketAddress addr6 = new BookieSocketAddress("127.0.0.6", 3181);
+        final BookieSocketAddress addr7 = new BookieSocketAddress("127.0.0.7", 3181);
+        final BookieSocketAddress addr8 = new BookieSocketAddress("127.0.0.8", 3181);
+        final BookieSocketAddress addr9 = new BookieSocketAddress("127.0.0.9", 3181);
+
+        final Set<BookieId> writableBookies = new HashSet<>();
+        writableBookies.add(addr1.toBookieId());
+        writableBookies.add(addr2.toBookieId());
+        writableBookies.add(addr3.toBookieId());
+        writableBookies.add(addr4.toBookieId());
+        writableBookies.add(addr5.toBookieId());
+        writableBookies.add(addr6.toBookieId());
+        writableBookies.add(addr7.toBookieId());
+        writableBookies.add(addr8.toBookieId());
+        writableBookies.add(addr9.toBookieId());
+
+        // add bookie node to resolver
+        StaticDNSResolver.reset();
+
+        final String rackName1 = NetworkTopology.DEFAULT_REGION + "/r1";
+        final String rackName2 = NetworkTopology.DEFAULT_REGION + "/r2";
+        final String rackName3 = NetworkTopology.DEFAULT_REGION + "/r3";
+
+        // update dns mapping
+        // add port for testing
+        StaticDNSResolver.addNodeToRack(addr1.getSocketAddress().getAddress().getHostAddress(), rackName1);
+        StaticDNSResolver.addNodeToRack(addr2.getSocketAddress().getAddress().getHostAddress(), rackName1);
+        StaticDNSResolver.addNodeToRack(addr3.getSocketAddress().getAddress().getHostAddress(), rackName1);
+        StaticDNSResolver.addNodeToRack(addr4.getSocketAddress().getAddress().getHostAddress(), rackName2);
+        StaticDNSResolver.addNodeToRack(addr5.getSocketAddress().getAddress().getHostAddress(), rackName2);
+        StaticDNSResolver.addNodeToRack(addr6.getSocketAddress().getAddress().getHostAddress(), rackName2);
+        StaticDNSResolver.addNodeToRack(addr7.getSocketAddress().getAddress().getHostAddress(), rackName3);
+        StaticDNSResolver.addNodeToRack(addr8.getSocketAddress().getAddress().getHostAddress(), rackName3);
+        StaticDNSResolver.addNodeToRack(addr9.getSocketAddress().getAddress().getHostAddress(), rackName3);
+        LOG.info("Set up static DNS Resolver.");
+        baseClientConf.setEnsemblePlacementPolicy(RackawareEnsemblePlacementPolicy.class);
+        baseClientConf.setProperty(REPP_DNS_RESOLVER_CLASS, StaticDNSResolver.class.getName());
+
+        startNewBookie();
+
+        final NavigableMap<Long, List<BookieId>> ensemble = new TreeMap<>();
+        // create failed ensemble
+        // expect that the ensemble will be replaced to
+        // [addr1, addr4, addr7, addr2, addr5, addr8, *addr6 (in /default-region/r2 bookies)*]
+        ensemble.put(0L, Arrays.asList(
+                addr1.toBookieId(), addr4.toBookieId(),
+                addr7.toBookieId(), addr2.toBookieId(),
+                addr5.toBookieId(), addr8.toBookieId(),
+                addr3.toBookieId()));
+        final LedgerMetadata lm1 = Mockito.mock(LedgerMetadata.class);
+        Mockito.doReturn(ensemble).when(lm1).getAllEnsembles();
+        Mockito.doReturn(ensembleSize).when(lm1).getEnsembleSize();
+        Mockito.doReturn(quorumSize).when(lm1).getWriteQuorumSize();
+        Mockito.doReturn(quorumSize).when(lm1).getAckQuorumSize();
+        final LedgerHandle lh1 = Mockito.mock(LedgerHandle.class);
+        Mockito.when(lh1.getLedgerMetadata()).thenReturn(lm1);
+
+        @Cleanup
+        final BookKeeper bookKeeper = Mockito.spy(new BookKeeper(baseClientConf));
+        Mockito.doReturn(true).when(bookKeeper).isClosed(Mockito.anyLong());
+
+        @Cleanup
+        final BookKeeperAdmin admin = Mockito.spy(new BookKeeperAdmin(baseClientConf));
+        Mockito.doAnswer(invocationOnMock -> {
+            final AsyncCallback.OpenCallback op = invocationOnMock.getArgument(1);
+            final CountDownLatch ctx = invocationOnMock.getArgument(2);
+            op.openComplete(BKException.Code.OK, lh1, ctx);
+            return null;
+        }).when(admin).asyncOpenLedger(Mockito.anyLong(), Mockito.any(), Mockito.any());
+        // expected return
+        final Map<Integer, BookieId> expectedMap = new HashMap<>();
+        expectedMap.put(6, addr6.toBookieId());
+        Mockito.doNothing().when(admin)
+                .replicateLedgerFragment(Mockito.any(), Mockito.any(), eq(expectedMap), Mockito.any());
+
+        final EnsemblePlacementPolicy policy = bookKeeper.getPlacementPolicy();
+        final Field topologyField = TopologyAwareEnsemblePlacementPolicy.class
+                .getDeclaredField("topology");
+        topologyField.setAccessible(true);
+        final NetworkTopology topology = Mockito.spy((NetworkTopology) topologyField.get(policy));
+        Mockito.doReturn(2).when(topology).getNumOfRacks();
+        topologyField.set(policy, topology);
+        final Field bookieAddressResolverField = TopologyAwareEnsemblePlacementPolicy.class
+                .getDeclaredField("bookieAddressResolver");
+        bookieAddressResolverField.setAccessible(true);
+        final BookieAddressResolver bookieAddressResolver =
+                Mockito.spy((BookieAddressResolver) bookieAddressResolverField.get(policy));
+        Mockito.doAnswer(invocationOnMock -> {
+            final BookieId bookieId = invocationOnMock.getArgument(0);
+            return new BookieSocketAddress(bookieId.getId());
+        }).when(bookieAddressResolver).resolve(Mockito.any());
+        bookieAddressResolverField.set(policy, bookieAddressResolver);
+
+        // add mock bookies to knownBookies
+        policy.onClusterChanged(writableBookies, Collections.emptySet());
+
+        // make sure that the ensemble is FAIL state
+        assertEquals(EnsemblePlacementPolicy.PlacementPolicyAdherence.FAIL,
+                policy.isEnsembleAdheringToPlacementPolicy(ensemble.get(0L), quorumSize, quorumSize));
+
+        final CorrectEnsemblePlacementCommand.CorrectEnsemblePlacementFlags flag =
+                new CorrectEnsemblePlacementCommand.CorrectEnsemblePlacementFlags();
+        flag.ledgerIds(Collections.singletonList(1L));
+        final CorrectEnsemblePlacementCommand cmd = new CorrectEnsemblePlacementCommand();
+        cmd.relocate(baseConf, flag, bookKeeper, admin);
+
+        Mockito.verify(admin, Mockito.times(1))
+                .replicateLedgerFragment(Mockito.any(), Mockito.any(), eq(expectedMap), Mockito.any());
+    }
+
+    private LedgerHandle createLedgerWithEntries(BookKeeper bk, int numOfEntries,
+                                                 int ensembleSize, int quorumSize) throws Exception {
+        LedgerHandle lh = bk.createLedger(ensembleSize, quorumSize, digestType, PASSWORD.getBytes());
+        final AtomicInteger rc = new AtomicInteger(BKException.Code.OK);
+        final CountDownLatch latch = new CountDownLatch(numOfEntries);
+
+        final AsyncCallback.AddCallback cb = (rccb, lh1, entryId, ctx) -> {
+            rc.compareAndSet(BKException.Code.OK, rccb);
+            latch.countDown();
+        };
+        for (int i = 0; i < numOfEntries; i++) {
+            lh.asyncAddEntry(("foobar" + i).getBytes(), cb, null);
+        }
+        if (!latch.await(30, TimeUnit.SECONDS)) {
+            throw new Exception("Entries took too long to add");
+        }
+        if (rc.get() != BKException.Code.OK) {
+            throw BKException.create(rc.get());
+        }
+        return lh;
+    }
+}
--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/TestRackawareEnsemblePlacementPolicy.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/TestRackawareEnsemblePlacementPolicy.java
@@ -21,6 +21,9 @@ import static org.apache.bookkeeper.client.RackawareEnsemblePlacementPolicyImpl.
 import static org.apache.bookkeeper.client.RackawareEnsemblePlacementPolicyImpl.shuffleWithMask;
 import static org.apache.bookkeeper.client.RoundRobinDistributionSchedule.writeSetFromValues;
 import static org.apache.bookkeeper.feature.SettableFeatureProvider.DISABLE_ALL;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import io.netty.util.HashedWheelTimer;
@@ -36,6 +39,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 import junit.framework.TestCase;
 import org.apache.bookkeeper.client.BKException.BKNotEnoughBookiesException;
 import org.apache.bookkeeper.client.BookieInfoReader.BookieInfo;
@@ -60,6 +64,10 @@ import org.apache.bookkeeper.test.TestStatsProvider;
 import org.apache.bookkeeper.test.TestStatsProvider.TestStatsLogger;
 import org.apache.bookkeeper.util.StaticDNSResolver;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -2386,5 +2394,141 @@ public class TestRackawareEnsemblePlacementPolicy extends TestCase {
         testAreAckedBookiesAdheringToPlacementPolicyHelper(2, 7, 3, 2, 7, 3, 3);
         testAreAckedBookiesAdheringToPlacementPolicyHelper(4, 6, 3, 2, 6, 3, 3);
         testAreAckedBookiesAdheringToPlacementPolicyHelper(5, 7, 5, 3, 7, 5, 2);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testReplaceToAdherePlacementPolicy() throws Exception {
+        final BookieSocketAddress addr1 = new BookieSocketAddress("127.0.0.1", 3181);
+        final BookieSocketAddress addr2 = new BookieSocketAddress("127.0.0.2", 3181);
+        final BookieSocketAddress addr3 = new BookieSocketAddress("127.0.0.3", 3181);
+        final BookieSocketAddress addr4 = new BookieSocketAddress("127.0.0.4", 3181);
+        final BookieSocketAddress addr5 = new BookieSocketAddress("127.0.0.5", 3181);
+        final BookieSocketAddress addr6 = new BookieSocketAddress("127.0.0.6", 3181);
+        final BookieSocketAddress addr7 = new BookieSocketAddress("127.0.0.7", 3181);
+        final BookieSocketAddress addr8 = new BookieSocketAddress("127.0.0.8", 3181);
+        final BookieSocketAddress addr9 = new BookieSocketAddress("127.0.0.9", 3181);
+
+        final String rackName1 = NetworkTopology.DEFAULT_REGION + "/r1";
+        final String rackName2 = NetworkTopology.DEFAULT_REGION + "/r2";
+        final String rackName3 = NetworkTopology.DEFAULT_REGION + "/r3";
+
+        // update dns mapping
+        StaticDNSResolver.addNodeToRack(addr1.getSocketAddress().getAddress().getHostAddress(), rackName1);
+        StaticDNSResolver.addNodeToRack(addr2.getSocketAddress().getAddress().getHostAddress(), rackName1);
+        StaticDNSResolver.addNodeToRack(addr3.getSocketAddress().getAddress().getHostAddress(), rackName1);
+        StaticDNSResolver.addNodeToRack(addr4.getSocketAddress().getAddress().getHostAddress(), rackName2);
+        StaticDNSResolver.addNodeToRack(addr5.getSocketAddress().getAddress().getHostAddress(), rackName2);
+        StaticDNSResolver.addNodeToRack(addr6.getSocketAddress().getAddress().getHostAddress(), rackName2);
+        StaticDNSResolver.addNodeToRack(addr7.getSocketAddress().getAddress().getHostAddress(), rackName3);
+        StaticDNSResolver.addNodeToRack(addr8.getSocketAddress().getAddress().getHostAddress(), rackName3);
+        StaticDNSResolver.addNodeToRack(addr9.getSocketAddress().getAddress().getHostAddress(), rackName3);
+
+        // Update cluster
+        final Set<BookieId> addrs = new HashSet<>();
+        addrs.add(addr1.toBookieId());
+        addrs.add(addr2.toBookieId());
+        addrs.add(addr3.toBookieId());
+        addrs.add(addr4.toBookieId());
+        addrs.add(addr5.toBookieId());
+        addrs.add(addr6.toBookieId());
+        addrs.add(addr7.toBookieId());
+        addrs.add(addr8.toBookieId());
+        addrs.add(addr9.toBookieId());
+
+        final ClientConfiguration newConf = new ClientConfiguration(conf);
+        newConf.setDiskWeightBasedPlacementEnabled(false);
+        newConf.setMinNumRacksPerWriteQuorum(2);
+        newConf.setEnforceMinNumRacksPerWriteQuorum(true);
+
+        repp.initialize(newConf, Optional.empty(), timer,
+                DISABLE_ALL, NullStatsLogger.INSTANCE, BookieSocketAddress.LEGACY_BOOKIEID_RESOLVER);
+        repp.withDefaultRack(NetworkTopology.DEFAULT_REGION_AND_RACK);
+
+        repp.onClusterChanged(addrs, new HashSet<>());
+        final Map<BookieId, BookieInfo> bookieInfoMap = new HashMap<>();
+        bookieInfoMap.put(addr1.toBookieId(), new BookieInfo(100L, 100L));
+        bookieInfoMap.put(addr2.toBookieId(), new BookieInfo(100L, 100L));
+        bookieInfoMap.put(addr3.toBookieId(), new BookieInfo(100L, 100L));
+        bookieInfoMap.put(addr4.toBookieId(), new BookieInfo(100L, 100L));
+        bookieInfoMap.put(addr5.toBookieId(), new BookieInfo(100L, 100L));
+        bookieInfoMap.put(addr6.toBookieId(), new BookieInfo(100L, 100L));
+        bookieInfoMap.put(addr7.toBookieId(), new BookieInfo(100L, 100L));
+        bookieInfoMap.put(addr8.toBookieId(), new BookieInfo(100L, 100L));
+        bookieInfoMap.put(addr9.toBookieId(), new BookieInfo(100L, 100L));
+
+        repp.updateBookieInfo(bookieInfoMap);
+
+        final Set<BookieId> excludeList = new HashSet<>();
+        final int ensembleSize = 7;
+        final int writeQuorumSize = 2;
+        final int ackQuorumSize = 2;
+
+        class BookieRackMatcher extends TypeSafeMatcher<BookieId> {
+            final List<String> expectedRacks;
+
+            public BookieRackMatcher(String... expectedRacks) {
+                this.expectedRacks = Arrays.asList(expectedRacks);
+            }
+
+            @Override
+            protected boolean matchesSafely(BookieId bookieId) {
+                return expectedRacks.contains(StaticDNSResolver.getRack(bookieId.toString().split(":")[0]));
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("expected racks " + expectedRacks);
+            }
+        }
+
+        final BookieRackMatcher rack1 = new BookieRackMatcher(rackName1);
+        final BookieRackMatcher rack2 = new BookieRackMatcher(rackName2);
+        final BookieRackMatcher rack3 = new BookieRackMatcher(rackName3);
+        final BookieRackMatcher rack12 = new BookieRackMatcher(rackName1, rackName2);
+        final BookieRackMatcher rack13 = new BookieRackMatcher(rackName1, rackName3);
+        final BookieRackMatcher rack23 = new BookieRackMatcher(rackName2, rackName3);
+        final BookieRackMatcher rack123 = new BookieRackMatcher(rackName1, rackName2, rackName3);
+        final Consumer<Pair<List<BookieId>, Matcher<Iterable<? extends BookieId>>>> test = (pair) -> {
+            // RackawareEnsemblePlacementPolicyImpl#isEnsembleAdheringToPlacementPolicy
+            // is not scope of this test case. So, use the method in assertion for convenience.
+            assertEquals(PlacementPolicyAdherence.FAIL,
+                    repp.isEnsembleAdheringToPlacementPolicy(pair.getLeft(), writeQuorumSize, ackQuorumSize));
+            final EnsemblePlacementPolicy.PlacementResult<List<BookieId>> result =
+                    repp.replaceToAdherePlacementPolicy(ensembleSize, writeQuorumSize, ackQuorumSize,
+                            excludeList, pair.getLeft());
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("input: {}, result: {}", pair.getLeft(), result.getResult());
+            }
+            assertEquals(PlacementPolicyAdherence.MEETS_STRICT, result.isAdheringToPolicy());
+            assertThat(result.getResult(), pair.getRight());
+        };
+
+        for (int i = 0; i < 1000; i++) {
+            test.accept(Pair.of(Arrays.asList(addr1.toBookieId(), addr4.toBookieId(), addr7.toBookieId(),
+                            addr2.toBookieId(), addr5.toBookieId(), addr8.toBookieId(), addr9.toBookieId()),
+                    // first, same, same, same, same, same, condition[0]
+                    contains(is(addr1.toBookieId()), is(addr4.toBookieId()), is(addr7.toBookieId()),
+                            is(addr2.toBookieId()), is(addr5.toBookieId()), is(addr8.toBookieId()),
+                            is(addr6.toBookieId()))));
+
+            test.accept(Pair.of(Arrays.asList(addr6.toBookieId(), addr4.toBookieId(), addr7.toBookieId(),
+                            addr2.toBookieId(), addr5.toBookieId(), addr8.toBookieId(), addr3.toBookieId()),
+                    // first, condition[0], same, same, same, same, same
+                    contains(is(addr6.toBookieId()), is(addr1.toBookieId()), is(addr7.toBookieId()),
+                            is(addr2.toBookieId()), is(addr5.toBookieId()), is(addr8.toBookieId()),
+                            is(addr3.toBookieId()))));
+
+            test.accept(Pair.of(Arrays.asList(addr1.toBookieId(), addr2.toBookieId(), addr3.toBookieId(),
+                            addr4.toBookieId(), addr5.toBookieId(), addr6.toBookieId(), addr7.toBookieId()),
+                    // first, candidate[0], same, same, candidate[0], same, same
+                    contains(is(addr1.toBookieId()), is(rack3), is(addr3.toBookieId()),
+                            is(addr4.toBookieId()), is(rack13), is(addr6.toBookieId()), is(addr7.toBookieId()))));
+
+            test.accept(Pair.of(Arrays.asList(addr1.toBookieId(), addr2.toBookieId(), addr4.toBookieId(),
+                            addr5.toBookieId(), addr7.toBookieId(), addr8.toBookieId(), addr9.toBookieId()),
+                    contains(is(addr1.toBookieId()), is(rack23), is(rack123), is(rack123),
+                            is(rack123), is(rack123), is(rack23))));
+        }
     }
 }
