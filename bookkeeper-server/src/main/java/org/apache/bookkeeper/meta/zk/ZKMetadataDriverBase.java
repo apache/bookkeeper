3246f6f0ccce7e14ace667399a3fc7f095147005
chenlin <1572139390@qq.com>
2021-12-22 22:54:48 +0800

    check tyle: remove unused import;

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/meta/zk/ZKMetadataDriverBase.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/meta/zk/ZKMetadataDriverBase.java
@@ -21,7 +21,6 @@ package org.apache.bookkeeper.meta.zk;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static org.apache.bookkeeper.util.BookKeeperConstants.AVAILABLE_NODE;
-import static org.apache.bookkeeper.util.BookKeeperConstants.DEFAULT_ZK_LEDGERS_ROOT_PATH;
 import static org.apache.bookkeeper.util.BookKeeperConstants.EMPTY_BYTE_ARRAY;
 import static org.apache.bookkeeper.util.BookKeeperConstants.ENABLE_HEALTH_CHECK;
 import static org.apache.bookkeeper.util.BookKeeperConstants.READONLY;
