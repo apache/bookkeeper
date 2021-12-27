c6f276791061468c8ea01d40e7e08c758a09b220
lushiji <lushiji@didiglobal.com>
2021-12-27 15:01:49 +0800

    check all Op requests and update log

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/client/PendingReadLacOp.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/client/PendingReadLacOp.java
@@ -156,7 +156,8 @@ class PendingReadLacOp implements ReadLacCallback {
         }
 
         if (numResponsesPending == 0 && !completed) {
-            LOG.info("While readLac ledger: " + ledgerId + " did not hear success responses from all of ensemble");
+            LOG.error("While readLac ledger: " + ledgerId + " did not hear success responses from " +
+                    "all of ensemble, coverageSet is: {}", coverageSet);
             cb.getLacComplete(lastSeenError, maxLac);
         }
     }
