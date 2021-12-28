ff2d0aa4e44732afde5850562fe6f1994335f049
wenbingshen <oliver.shen999@gmail.com>
2021-12-28 11:47:24 +0800

    fix test failed

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/AbstractLogCompactor.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/AbstractLogCompactor.java
@@ -28,6 +28,7 @@ import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import org.apache.bookkeeper.conf.ServerConfiguration;
 
@@ -72,21 +73,24 @@ public abstract class AbstractLogCompactor {
     // If the compactor is being stopped by other threads,
     // and the GC thread is still limited, the compact task will be stopped.
     public void acquire(int permits) throws IOException {
-        Future<?> acquire = rateAcquireExecutor.submit(() -> {
-            throttler.acquire(permits);
-        });
+        long timeout = 100;
+        if (!throttler.tryAcquire(permits, timeout, TimeUnit.MILLISECONDS)) {
+            long start = System.currentTimeMillis();
+            Future<?> acquire = rateAcquireExecutor.submit(() -> {
+                throttler.acquire(permits);
+            });
 
-        while (!acquire.isDone()) {
-            if (acquire.isCancelled() || shutting.get()) {
-                acquire.cancel(true);
-                cancelled.set(true);
-                throw new IOException("Failed to get permits may be compactor has been shutting down");
-            }
-
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                // ignore
+            while (!acquire.isDone()) {
+                if (acquire.isCancelled() || shutting.get()) {
+                    acquire.cancel(true);
+                    cancelled.set(true);
+                    throw new IOException("Failed to get permits takes " + (System.currentTimeMillis() - start) + " ms may be compactor has been shutting down");
+                }
+                try {
+                    TimeUnit.MILLISECONDS.sleep(timeout);
+                } catch (InterruptedException ex) {
+                    // ignore
+                }
             }
         }
     }
@@ -116,6 +120,10 @@ public abstract class AbstractLogCompactor {
         // acquire. if bybytes: bytes of this entry; if byentries: 1.
         void acquire(int permits) {
             rateLimiter.acquire(this.isThrottleByBytes ? permits : 1);
+        }
+
+        boolean tryAcquire(int permits, long timeout, TimeUnit unit) {
+            return rateLimiter.tryAcquire(this.isThrottleByBytes ? permits : 1, timeout, unit);
         }
     }
 
