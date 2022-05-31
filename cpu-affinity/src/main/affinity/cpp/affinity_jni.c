apache/bookkeeper

commit f1ff8b4a307e09594fc663625c50318dde5fd7e4

[build] support circe-checksum and cpu-affinity build on Windows (#2549)
Co-authored-by: dongcao <dongcao@trip.com>

cpu-affinity/src/main/affinity/cpp/affinity_jni.c
@@ -44,6 +44,8 @@ static int set_affinity(int cpuid) { return NOT_IMPLEMENTED; }
 
 static const int IS_AVAILABLE = 0;
 
+#define strerror_r(errno,buf,len) strerror_s(buf,len,errno)
+
 #endif
 
 #include <unistd.h>
@@ -60,7 +62,11 @@ static const int IS_AVAILABLE = 0;
  */
 JNIEXPORT jboolean JNICALL
 Java_org_apache_bookkeeper_common_util_affinity_impl_CpuAffinityJni_isRoot(JNIEnv *env, jclass cls) {
+#ifdef __linux__
     return getuid() == 0;
+#else
+    return 0;
+#endif
 }
 
 /*

circe-checksum/pom.xml
@@ -237,6 +237,43 @@
         </plugins>
       </build>
     </profile>
+    <profile>
+      <id>Windows</id>
+      <activation>
+        <os>
+          <family>Windows</family>
+        </os>
+      </activation>
+      <build>
+        <plugins>
+          <plugin>
+            <groupId>com.github.maven-nar</groupId>
+            <artifactId>nar-maven-plugin</artifactId>
+            <version>${nar-maven-plugin.version}</version>
+            <extensions>true</extensions>
+            <configuration>
+              <runtime>${nar.runtime}</runtime>
+              <output>circe-checksum</output>
+              <libraries>
+                <library>
+                  <type>jni</type>
+                  <narSystemPackage>com.scurrilous.circe.checksum</narSystemPackage>
+                </library>
+              </libraries>
+              <cpp>
+                <optionSet>${nar.cpp.optionSet}</optionSet>
+                <exceptions>false</exceptions>
+                <rtti>false</rtti>
+                <optimize>full</optimize>
+              </cpp>
+              <linker>
+                <name>g++</name>
+              </linker>
+            </configuration>
+          </plugin>
+        </plugins>
+      </build>
+    </profile>
   </profiles>
 
 </project>

cpu-affinity/pom.xml
@@ -221,6 +221,43 @@
         </plugins>
       </build>
     </profile>
+    <profile>
+      <id>Windows</id>
+      <activation>
+        <os>
+          <family>Windows</family>
+        </os>
+      </activation>
+      <build>
+        <plugins>
+          <plugin>
+            <groupId>com.github.maven-nar</groupId>
+            <artifactId>nar-maven-plugin</artifactId>
+            <version>${nar-maven-plugin.version}</version>
+            <extensions>true</extensions>
+            <configuration>
+              <runtime>${nar.runtime}</runtime>
+              <output>cpu-affinity</output>
+              <libraries>
+                <library>
+                  <type>jni</type>
+                  <narSystemPackage>org.apache.bookkeeper.utils.affinity</narSystemPackage>
+                </library>
+              </libraries>
+              <cpp>
+                <optionSet>${nar.cpp.optionSet}</optionSet>
+                <exceptions>false</exceptions>
+                <rtti>false</rtti>
+                <optimize>full</optimize>
+              </cpp>
+              <linker>
+                <name>g++</name>
+              </linker>
+            </configuration>
+          </plugin>
+        </plugins>
+      </build>
+    </profile>
   </profiles>
 
 </project>

commit 77441e6f2602632b44b7d4a01c6d122cdc2a1ced

[build] [windows] fix broken master code compile error strerror_s (#3300)
### Motivation
master compile is broken by #2549 

### Changes

Add windows `#ifdef _WIN32` directive on `define`. Test on my m1 mac and windows 10.

cpu-affinity/src/main/affinity/cpp/affinity_jni.c
@@ -44,6 +44,10 @@ static int set_affinity(int cpuid) { return NOT_IMPLEMENTED; }
 
 static const int IS_AVAILABLE = 0;
 
+#endif
+
+#ifdef _WIN32
+
 #define strerror_r(errno,buf,len) strerror_s(buf,len,errno)
 
 #endif

native-io/src/main/native-io-jni/cpp/native_io_jni.c
@@ -28,6 +28,12 @@
 
 #include <org_apache_bookkeeper_common_util_nativeio_NativeIOJni.h>
 
+#ifdef _WIN32
+
+#define strerror_r(errno,buf,len) strerror_s(buf,len,errno)
+
+#endif
+
 static void throwExceptionWithErrno(JNIEnv* env, const char* message) {
     char err_msg[1024];
     strerror_r(errno, err_msg, sizeof(err_msg));