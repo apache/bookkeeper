70bbc3830f07691cbdb87fe03958196fafc0effb
Yong Zhang <zhangyong1025.zy@gmail.com>
2022-09-12 18:49:32 +0800

    Fix the deadlock when only using io thread to handle request (#3480)
    * Fix the deadlock when only using io thread to handle request
    ---
    
    *Motivation*
    
    If user don't configure the ReadWorker thread pool, the reqeust will
    process with io thread. We cannot call await() from an IO thread,
    if the socket buffer is full, that blocking call would cause a deadlock.
    
    *Modification*
    
    - only wait the promise when the thread is not io thread
    
    * Fix the style issue

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/PacketProcessorBase.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/PacketProcessorBase.java
@@ -18,6 +18,7 @@
 package org.apache.bookkeeper.proto;
 
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import java.util.concurrent.TimeUnit;
 import org.apache.bookkeeper.proto.BookieProtocol.Request;
 import org.apache.bookkeeper.stats.OpStatsLogger;
@@ -136,7 +137,10 @@ abstract class PacketProcessorBase<T extends Request> extends SafeRunnable {
      */
     protected void sendResponseAndWait(int rc, Object response, OpStatsLogger statsLogger) {
         try {
-            channel.writeAndFlush(response).await();
+            ChannelFuture future = channel.writeAndFlush(response);
+            if (!channel.eventLoop().inEventLoop()) {
+                future.await();
+            }
         } catch (InterruptedException e) {
             return;
         }
