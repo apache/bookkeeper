8aaf3d93b5873cfe0bbe5d4196df40fb730b008b
Andrey Yegorov <8622884+dlg99@users.noreply.github.com>
2022-10-07 19:20:27 -0700

    LedgerOpenOp: Do not call blocking close() in the callback (#3513)
    * Do not call blocking close() in the callback
         * CR feedback, remove lombok from LedgerOpenOp

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/client/LedgerOpenOp.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/client/LedgerOpenOp.java
@@ -129,17 +129,11 @@ class LedgerOpenOp {
         initiate();
     }
 
-    private void closeLedgerHandle() {
-        try {
-            if (lh != null) {
-                lh.close();
-            }
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            LOG.info("InterruptedException while closing ledger {}", ledgerId, e);
-        } catch (BKException e) {
-            LOG.warn("BKException while closing ledger {} ", ledgerId, e);
+    private CompletableFuture<Void> closeLedgerHandleAsync() {
+        if (lh != null) {
+            return lh.closeAsync();
         }
+        return CompletableFuture.completedFuture(null);
     }
 
     private void openWithMetadata(Versioned<LedgerMetadata> versionedMetadata) {
@@ -209,10 +203,13 @@ class LedgerOpenOp {
                     if (rc == BKException.Code.OK) {
                         openComplete(BKException.Code.OK, lh);
                     } else if (rc == BKException.Code.UnauthorizedAccessException) {
-                        closeLedgerHandle();
-                        openComplete(BKException.Code.UnauthorizedAccessException, null);
+                        closeLedgerHandleAsync().whenComplete((r, ex) -> {
+                            if (ex != null) {
+                                LOG.error("Ledger {} close failed", ledgerId, ex);
+                            }
+                            openComplete(BKException.Code.UnauthorizedAccessException, null);
+                        });
                     } else {
-                        closeLedgerHandle();
                         openComplete(bk.getReturnRc(BKException.Code.LedgerRecoveryException), null);
                     }
                 }
@@ -227,8 +224,12 @@ class LedgerOpenOp {
                 public void readLastConfirmedComplete(int rc,
                         long lastConfirmed, Object ctx) {
                     if (rc != BKException.Code.OK) {
-                        closeLedgerHandle();
-                        openComplete(bk.getReturnRc(BKException.Code.ReadException), null);
+                        closeLedgerHandleAsync().whenComplete((r, ex) -> {
+                            if (ex != null) {
+                                LOG.error("Ledger {} close failed", ledgerId, ex);
+                            }
+                            openComplete(bk.getReturnRc(BKException.Code.ReadException), null);
+                        });
                     } else {
                         lh.lastAddConfirmed = lh.lastAddPushed = lastConfirmed;
                         openComplete(BKException.Code.OK, lh);
