5037159d8dbee3dca098565fdf12b2dd22eec51d
Matteo Merli <mmerli@apache.org>
2022-10-31 21:05:42 -0700

    Avoid extra buffer to prepend frame size (#3560)
    * Avoid extra buffer to prepend frame size
    
    * Fixed checkstyle
    
    * Fixed touch methods on ReadResponse
    
    * Fixed frame size for protobuf requests
    
    * Removed unwanted changes
    
    * Fixed AuthResponse in v2 protocol
    
    * Fixed test
    
    (cherry picked from commit 02be9d2ee28cd7c4a1895fc8494db96381484a20)

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/BookieProtocol.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/proto/BookieProtocol.java
@@ -466,7 +466,8 @@ public interface BookieProtocol {
 
         @Override
         public ReferenceCounted retain() {
-            return data.retain();
+            data.retain();
+            return this;
         }
 
         @Override
@@ -476,12 +477,14 @@ public interface BookieProtocol {
 
         @Override
         public ReferenceCounted touch() {
-            return data.touch();
+            data.touch();
+            return this;
         }
 
         @Override
         public ReferenceCounted touch(Object hint) {
-            return data.touch(hint);
+            data.touch(hint);
+            return this;
         }
 
         @Override
