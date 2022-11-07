e1d74543a3dcc1c43c5f74c211ef71455b01025b
wenbingshen <oliver.shen999@gmail.com>
2022-11-07 23:50:16 +0800

    fix checkstyle

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/proto/GetBookieInfoProcessorV3Test.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/proto/GetBookieInfoProcessorV3Test.java
@@ -24,6 +24,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
 import io.netty.channel.Channel;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
