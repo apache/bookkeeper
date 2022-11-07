f3d9139653ef9dfcbbeb7bd0e49f83d27681a5d5
leolinchen <leolinchen@tencent.com>
2022-11-07 10:28:38 +0800

    When unpersistedBytes.get() >= unpersistedBytesBound, call forceWrite.

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/BufferedChannel.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/BufferedChannel.java
@@ -134,7 +134,7 @@ public class BufferedChannel extends BufferedReadChannel implements Closeable {
             if (doRegularFlushes) {
                 unpersistedBytes.addAndGet(copied);
                 if (unpersistedBytes.get() >= unpersistedBytesBound) {
-                    flush();
+                    forceWrite(false);
                     shouldForceWrite = true;
                 }
             }
