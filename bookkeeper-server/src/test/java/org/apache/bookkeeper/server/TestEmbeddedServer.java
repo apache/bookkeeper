830116280ff6fb04ef330a1f01e0a009060ae507
Diego Salvi <lothruin.mirwen@gmail.com>
2022-11-09 17:19:05 +0100

    Issue #3488 Create an EmbeddedSever extracting code from Main class and adding a builder pattern (#3489)

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/server/TestEmbeddedServer.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/server/TestEmbeddedServer.java
@@ -1,4 +1,5 @@
-/*
+/**
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -15,11 +16,15 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ *
  */
 
 package org.apache.bookkeeper.server;
 
+import static org.apache.bookkeeper.bookie.BookKeeperServerStats.LD_INDEX_SCOPE;
+import static org.apache.bookkeeper.bookie.BookKeeperServerStats.LD_LEDGER_SCOPE;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
@@ -31,17 +36,26 @@ import static org.powermock.api.mockito.PowerMockito.whenNew;
 import java.io.IOException;
 import org.apache.bookkeeper.bookie.BookieImpl;
 import org.apache.bookkeeper.bookie.BookieResources;
+import org.apache.bookkeeper.bookie.LedgerDirsManager;
 import org.apache.bookkeeper.bookie.LegacyCookieValidation;
+import org.apache.bookkeeper.bookie.UncleanShutdownDetectionImpl;
 import org.apache.bookkeeper.common.allocator.ByteBufAllocatorWithOomHandler;
 import org.apache.bookkeeper.common.component.LifecycleComponentStack;
 import org.apache.bookkeeper.conf.ServerConfiguration;
+import org.apache.bookkeeper.discover.RegistrationManager;
+import org.apache.bookkeeper.meta.LedgerManagerFactory;
 import org.apache.bookkeeper.meta.NullMetadataBookieDriver;
+import org.apache.bookkeeper.meta.NullMetadataBookieDriver.NullLedgerManagerFactory;
+import org.apache.bookkeeper.meta.NullMetadataBookieDriver.NullRegistrationManager;
 import org.apache.bookkeeper.net.BookieSocketAddress;
 import org.apache.bookkeeper.proto.BookieServer;
 import org.apache.bookkeeper.server.component.ServerLifecycleComponent;
 import org.apache.bookkeeper.server.conf.BookieConfiguration;
 import org.apache.bookkeeper.server.service.BookieService;
+import org.apache.bookkeeper.stats.NullStatsProvider;
 import org.apache.bookkeeper.stats.StatsLogger;
+import org.apache.bookkeeper.stats.StatsProvider;
+import org.apache.bookkeeper.util.DiskChecker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
@@ -49,11 +63,11 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
- * Unit test of {@link Main}.
+ * Unit test of {@link EmbeddedServer}.
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({BookieService.class, BookieResources.class, Main.class})
-public class TestMain {
+@PrepareForTest({BookieService.class, BookieResources.class, EmbeddedServer.class})
+public class TestEmbeddedServer {
 
     static class TestComponent extends ServerLifecycleComponent {
 
@@ -84,6 +98,7 @@ public class TestMain {
                 PowerMockito.mock(ByteBufAllocatorWithOomHandler.class));
 
         ServerConfiguration serverConf = new ServerConfiguration()
+            .setAllowLoopback(true)
             .setAutoRecoveryDaemonEnabled(false)
             .setHttpServerEnabled(false)
             .setExtraServerComponents(new String[] { TestComponent.class.getName() });
@@ -102,9 +117,86 @@ public class TestMain {
         when(mockServer.getLocalAddress()).thenReturn(bookieAddress);
         when(mockServer.getBookieId()).thenReturn(bookieAddress.toBookieId());
 
-        LifecycleComponentStack stack = Main.buildBookieServer(conf);
+        EmbeddedServer server = EmbeddedServer.builder(conf).build();
+        LifecycleComponentStack stack = server.getLifecycleComponentStack();
         assertEquals(7, stack.getNumComponents());
         assertTrue(stack.getComponent(6) instanceof TestComponent);
+
+        stack.start();
+        verify(mockServer, times(1)).start();
+
+        stack.stop();
+
+        stack.close();
+        verify(mockServer, times(1)).shutdown();
+    }
+
+    @Test
+    public void testBuildBookieServerCustomComponents() throws Exception {
+
+        ServerConfiguration serverConf = new ServerConfiguration()
+                .setAllowLoopback(true)
+                .setAutoRecoveryDaemonEnabled(false)
+                .setHttpServerEnabled(false)
+                .setExtraServerComponents(new String[]{TestComponent.class.getName()});
+        BookieConfiguration conf = new BookieConfiguration(serverConf);
+
+        StatsProvider statsProvider = new NullStatsProvider();
+        StatsLogger rootStatsLogger = statsProvider.getStatsLogger("");
+        RegistrationManager registrationManager = new NullRegistrationManager();
+        LedgerManagerFactory ledgerManagerFactory = new NullLedgerManagerFactory();
+
+        DiskChecker diskChecker = BookieResources.createDiskChecker(serverConf);
+
+        LedgerDirsManager ledgerDirsManager = BookieResources.createLedgerDirsManager(
+                conf.getServerConf(), diskChecker, rootStatsLogger.scope(LD_LEDGER_SCOPE));
+
+        LedgerDirsManager indexDirsManager = BookieResources.createIndexDirsManager(
+                conf.getServerConf(), diskChecker, rootStatsLogger.scope(LD_INDEX_SCOPE), ledgerDirsManager);
+
+        UncleanShutdownDetectionImpl uncleanShutdownDetection = new UncleanShutdownDetectionImpl(ledgerDirsManager);
+
+        ByteBufAllocatorWithOomHandler byteBufFromResources = PowerMockito.mock(ByteBufAllocatorWithOomHandler.class);
+        ByteBufAllocatorWithOomHandler byteBuf = PowerMockito.mock(ByteBufAllocatorWithOomHandler.class);
+
+        PowerMockito.mockStatic(BookieResources.class);
+        when(BookieResources.createMetadataDriver(any(), any())).thenReturn(new NullMetadataBookieDriver());
+        when(BookieResources.createAllocator(any())).thenReturn(byteBufFromResources);
+
+        whenNew(BookieImpl.class).withAnyArguments().thenReturn(PowerMockito.mock(BookieImpl.class));
+        whenNew(LegacyCookieValidation.class)
+                .withAnyArguments().thenReturn(PowerMockito.mock(LegacyCookieValidation.class));
+
+        BookieServer mockServer = PowerMockito.mock(BookieServer.class);
+        whenNew(BookieServer.class)
+                .withAnyArguments()
+                .thenReturn(mockServer);
+
+        BookieSocketAddress bookieAddress = new BookieSocketAddress("127.0.0.1", 1281);
+        when(mockServer.getLocalAddress()).thenReturn(bookieAddress);
+        when(mockServer.getBookieId()).thenReturn(bookieAddress.toBookieId());
+
+        EmbeddedServer server = EmbeddedServer.builder(conf)
+                .statsProvider(statsProvider)
+                .registrationManager(registrationManager)
+                .ledgerManagerFactory(ledgerManagerFactory)
+                .diskChecker(diskChecker)
+                .ledgerDirsManager(ledgerDirsManager)
+                .indexDirsManager(indexDirsManager)
+                .allocator(byteBuf)
+                .uncleanShutdownDetection(uncleanShutdownDetection)
+                .build();
+
+        assertSame(statsProvider, server.getStatsProvider());
+        assertSame(registrationManager, server.getRegistrationManager());
+        assertSame(ledgerManagerFactory, server.getLedgerManagerFactory());
+        assertSame(diskChecker, server.getDiskChecker());
+        assertSame(ledgerDirsManager, server.getLedgerDirsManager());
+        assertSame(indexDirsManager, server.getIndexDirsManager());
+
+        LifecycleComponentStack stack = server.getLifecycleComponentStack();
+        assertEquals(3, stack.getNumComponents());
+        assertTrue(stack.getComponent(2) instanceof TestComponent);
 
         stack.start();
         verify(mockServer, times(1)).start();
@@ -122,6 +214,7 @@ public class TestMain {
             .thenReturn(new NullMetadataBookieDriver());
 
         ServerConfiguration serverConf = new ServerConfiguration()
+            .setAllowLoopback(true)
             .setAutoRecoveryDaemonEnabled(false)
             .setHttpServerEnabled(false)
             .setExtraServerComponents(new String[] { "bad-server-component"})
@@ -141,7 +234,7 @@ public class TestMain {
         when(mockServer.getLocalAddress()).thenReturn(bookieAddress);
         when(mockServer.getBookieId()).thenReturn(bookieAddress.toBookieId());
 
-        LifecycleComponentStack stack = Main.buildBookieServer(conf);
+        LifecycleComponentStack stack = EmbeddedServer.builder(conf).build().getLifecycleComponentStack();
         assertEquals(6, stack.getNumComponents());
 
         stack.start();
@@ -160,6 +253,7 @@ public class TestMain {
             .thenReturn(new NullMetadataBookieDriver());
 
         ServerConfiguration serverConf = new ServerConfiguration()
+            .setAllowLoopback(true)
             .setAutoRecoveryDaemonEnabled(false)
             .setHttpServerEnabled(false)
             .setExtraServerComponents(new String[] { "bad-server-component"})
@@ -180,11 +274,10 @@ public class TestMain {
         when(mockServer.getBookieId()).thenReturn(bookieAddress.toBookieId());
 
         try {
-            Main.buildBookieServer(conf);
+            EmbeddedServer.builder(conf).build().getLifecycleComponentStack();
             fail("Should fail to start bookie server if `ignoreExtraServerComponentsStartupFailures` is set to false");
         } catch (RuntimeException re) {
             assertTrue(re.getCause() instanceof ClassNotFoundException);
         }
     }
-
 }
