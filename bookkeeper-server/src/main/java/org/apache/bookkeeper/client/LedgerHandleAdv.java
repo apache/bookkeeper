0de2a67e03ccbe8bae78afc227100472ce8ef41d
zhaohaidao <zhaohaidao2008@hotmail.com>
2022-11-14 09:27:55 +0800

    Apply recycle logic during add entry creation but ledger close to LedgerHandleAdv (#3621)
    Descriptions of the changes in this PR:
    Apply recycle logic during add entry creation but ledger close to LedgerHandleAdv
    
    ### Motivation
    This pr is a supplement to #3321
    If I understand correctly. these recycled logic should be applied to LedgerHandleAdv

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/client/LedgerHandleAdv.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/client/LedgerHandleAdv.java
@@ -248,6 +248,7 @@ public class LedgerHandleAdv extends LedgerHandle implements WriteAdvHandle {
                         LOG.warn("Attempt to add to closed ledger: {}", ledgerId);
                         op.cb.addCompleteWithLatency(BKException.Code.LedgerClosedException,
                                 LedgerHandleAdv.this, op.getEntryId(), 0, op.ctx);
+                        op.recyclePendAddOpObject();
                     }
                     @Override
                     public String toString() {
@@ -258,6 +259,7 @@ public class LedgerHandleAdv extends LedgerHandle implements WriteAdvHandle {
                 op.cb.addCompleteWithLatency(BookKeeper.getReturnRc(clientCtx.getBookieClient(),
                                                                     BKException.Code.InterruptedException),
                         LedgerHandleAdv.this, op.getEntryId(), 0, op.ctx);
+                op.recyclePendAddOpObject();
             }
             return;
         }
