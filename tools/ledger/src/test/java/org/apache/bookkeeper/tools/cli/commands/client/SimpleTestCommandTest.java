c199dd5a37b26aba8227f6bb7eeca1fb37e421ed
Yan Zhao <horizonzy@apache.org>
2022-11-30 09:34:43 +0800

    Enhance simple test (#3675)
    ### Motivation
    Fixes #3670

--- tools/ledger/src/test/java/org/apache/bookkeeper/tools/cli/commands/client/SimpleTestCommandTest.java
+++ tools/ledger/src/test/java/org/apache/bookkeeper/tools/cli/commands/client/SimpleTestCommandTest.java
@@ -28,15 +28,25 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.UnpooledByteBufAllocator;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicLong;
 import org.apache.bookkeeper.client.api.CreateBuilder;
 import org.apache.bookkeeper.client.api.DeleteBuilder;
 import org.apache.bookkeeper.client.api.DigestType;
+import org.apache.bookkeeper.client.api.LedgerEntry;
+import org.apache.bookkeeper.client.api.OpenBuilder;
+import org.apache.bookkeeper.client.api.ReadHandle;
 import org.apache.bookkeeper.client.api.WriteHandle;
+import org.apache.bookkeeper.client.impl.LedgerEntriesImpl;
+import org.apache.bookkeeper.client.impl.LedgerEntryImpl;
 import org.apache.bookkeeper.common.concurrent.FutureUtils;
 import org.apache.bookkeeper.tools.cli.helpers.ClientCommandTestBase;
 import org.junit.Test;
@@ -53,8 +63,7 @@ public class SimpleTestCommandTest extends ClientCommandTestBase {
             "-e", "5",
             "-w", "3",
             "-a", "3",
-            "-n", "10",
-            "-c");
+            "-n", "10");
     }
 
     @Test
@@ -63,8 +72,7 @@ public class SimpleTestCommandTest extends ClientCommandTestBase {
             "--ensemble-size", "5",
             "--write-quorum-size", "3",
             "--ack-quorum-size", "3",
-            "--num-entries", "10",
-            "-c");
+            "--num-entries", "10");
     }
 
     @SuppressWarnings("unchecked")
@@ -82,6 +90,29 @@ public class SimpleTestCommandTest extends ClientCommandTestBase {
         when(createBuilder.withPassword(any(byte[].class))).thenReturn(createBuilder);
         when(createBuilder.execute()).thenReturn(CompletableFuture.completedFuture(wh));
         when(mockBk.newCreateLedgerOp()).thenReturn(createBuilder);
+
+        List<LedgerEntry> entries = new ArrayList<>();
+        byte[] data = new byte[100]; // test data
+        Random random = new Random(0);
+        for (int i = 0; i < data.length; i++) {
+            data[i] = (byte) (random.nextInt(26) + 65);
+        }
+        for (int i = 0; i < 10; i++) {
+            ByteBuf buffer = UnpooledByteBufAllocator.DEFAULT.heapBuffer(100);
+            buffer.writeBytes(data);
+            entries.add(LedgerEntryImpl.create(counter.get(), i, data.length, buffer));
+        }
+
+        LedgerEntriesImpl ledgerEntries = LedgerEntriesImpl.create(entries);
+
+        ReadHandle rh = mock(ReadHandle.class);
+        when(rh.read(anyLong(), anyLong())).thenReturn(ledgerEntries);
+        OpenBuilder openBuilder = mock(OpenBuilder.class);
+        when(openBuilder.withLedgerId(anyLong())).thenReturn(openBuilder);
+        when(openBuilder.withDigestType(any())).thenReturn(openBuilder);
+        when(openBuilder.withPassword(any())).thenReturn(openBuilder);
+        when(openBuilder.execute()).thenReturn(CompletableFuture.completedFuture(rh));
+        when(mockBk.newOpenLedgerOp()).thenReturn(openBuilder);
 
         DeleteBuilder deleteBuilder = mock(DeleteBuilder.class);
         when(deleteBuilder.withLedgerId(anyLong())).thenReturn(deleteBuilder);
@@ -103,11 +134,16 @@ public class SimpleTestCommandTest extends ClientCommandTestBase {
         verify(createBuilder, times(1)).withPassword(eq(new byte[0]));
         verify(createBuilder, times(1)).execute();
 
+        verify(openBuilder, times(1)).withLedgerId(eq(0L));
+        verify(openBuilder, times(1)).execute();
+
         verify(deleteBuilder, times(1)).withLedgerId(eq(0L));
         verify(deleteBuilder, times(1)).execute();
 
         // verify appends
-        verify(wh, times(10)).append(eq(new byte[100]));
+        verify(wh, times(10)).append(eq(data));
+
+        verify(rh, times(1)).read(anyLong(), anyLong());
     }
 
 }
