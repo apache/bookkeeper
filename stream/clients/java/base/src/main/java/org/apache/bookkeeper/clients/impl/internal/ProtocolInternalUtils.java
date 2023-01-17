0ec24318a3bb01b6428a7db9bb82bb54f6269c19
ZhangJian He <shoothzj@gmail.com>
2023-01-17 11:16:35 +0800

    unify SLASHSTAR_STYLE for licenses in Java files

--- stream/clients/java/base/src/main/java/org/apache/bookkeeper/clients/impl/internal/ProtocolInternalUtils.java
+++ stream/clients/java/base/src/main/java/org/apache/bookkeeper/clients/impl/internal/ProtocolInternalUtils.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.bookkeeper.clients.impl.internal;
 
 import static com.google.common.base.Preconditions.checkState;
