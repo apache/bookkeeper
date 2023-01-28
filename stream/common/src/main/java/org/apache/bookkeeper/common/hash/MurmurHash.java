3c5e34d3ec97288cff1b6f452c50a8ff8fd7ae0b
ZhangJian He <shoothzj@gmail.com>
2023-01-28 16:15:39 +0800

    unify SLASHSTAR_STYLE for licenses in Java files (#3741)
    ### Motivation
    Use SLASHSTAR_STYLE to avoid formatting unnecessary changes.
    
    See also: 
    - https://github.com/apache/bookkeeper/pull/3717
    - https://github.com/apache/pulsar/pull/18177
    - https://lists.apache.org/thread/gwy3ycgsq24r784mk3yv6cbppmbfxgjd
    
    ### Changes
    Use SLASHSTAR_STYLE instead of javadoc style.

--- stream/common/src/main/java/org/apache/bookkeeper/common/hash/MurmurHash.java
+++ stream/common/src/main/java/org/apache/bookkeeper/common/hash/MurmurHash.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.bookkeeper.common.hash;
 
 import io.netty.buffer.ByteBuf;
--- stream/proto/src/main/java/org/apache/bookkeeper/stream/protocol/util/ProtoUtils.java
+++ stream/proto/src/main/java/org/apache/bookkeeper/stream/protocol/util/ProtoUtils.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.bookkeeper.stream.protocol.util;
 
 import static com.google.common.base.Preconditions.checkArgument;
