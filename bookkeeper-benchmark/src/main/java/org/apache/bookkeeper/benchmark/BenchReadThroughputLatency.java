13b2fa80e5beff3dbfe91cca52a579b4006b7891
chenhang <chenhang@apache.org>
2023-03-21 12:11:02 +0800

    make description clear

--- bookkeeper-benchmark/src/main/java/org/apache/bookkeeper/benchmark/BenchReadThroughputLatency.java
+++ bookkeeper-benchmark/src/main/java/org/apache/bookkeeper/benchmark/BenchReadThroughputLatency.java
@@ -157,8 +157,7 @@ public class BenchReadThroughputLatency {
         options.addOption("password", true, "Password used to access ledgers (default 'benchPasswd')");
         options.addOption("zookeeper", true, "Zookeeper ensemble, default \"localhost:2181\"");
         options.addOption("sockettimeout", true, "Socket timeout for bookkeeper client. In seconds. Default 5");
-        options.addOption("useV2", false,
-            "Whether use V2 protocol to read ledgers from the bookie server. Default is false");
+        options.addOption("useV2", false, "Whether use V2 protocol to read ledgers from the bookie server.");
         options.addOption("help", false, "This message");
 
         CommandLineParser parser = new PosixParser();
