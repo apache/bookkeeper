2f3ab1c02383b7f1334569542c8471290649aa37
houxiaoyu <houxiaoyu@apache.org>
2023-07-21 10:33:46 +0800

    Fix deletedLedgers count (#4026)
    (cherry picked from commit 3bf08fef12979bf3cff49ee953899f26c6281ad9)

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/storage/ldb/LedgerMetadataIndex.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/storage/ldb/LedgerMetadataIndex.java
@@ -233,6 +233,7 @@ public class LedgerMetadataIndex implements Closeable {
         for (Long ledgerId : pendingDeletedLedgers) {
             key.set(ledgerId);
             ledgersDb.delete(key.array);
+            ++deletedLedgers;
         }
 
         if (log.isDebugEnabled()) {
