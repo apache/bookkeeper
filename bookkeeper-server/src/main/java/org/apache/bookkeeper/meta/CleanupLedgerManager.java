39275588122d7bb2e554c1bd3c45aa7119b2c497
Yong Zhang <zhangyong1025.zy@gmail.com>
2023-11-01 22:36:26 +0800

    Remove the unused logs in the CleanupLedgerManager.recordPromise (#4121)
    ---
    
    ### Motivation
    
    In the cleanup ledger manager, we just record every future and remove
    them when the future complete. Seems we don't need to print the logs
    
    (cherry picked from commit bf9a5cf3ee457797542701f0f336bede227c7fbd)

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/meta/CleanupLedgerManager.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/meta/CleanupLedgerManager.java
@@ -116,9 +116,6 @@ public class CleanupLedgerManager implements LedgerManager {
         futures.add(promise);
         promise.whenComplete((result, exception) -> {
             futures.remove(promise);
-            if (exception != null) {
-                log.error("Failed on operating ledger metadata: {}", BKException.getExceptionCode(exception));
-            }
         });
     }
 
