d2b9c7b225baf4afe7a92aaab66a055fb857ab60
zymap <zhangyong1025.zy@gmail.com>
2023-12-19 16:43:31 +0800

    Bump version to the next develop version

--- stream/clients/python/setup.py
+++ stream/clients/python/setup.py
@@ -19,7 +19,7 @@ import setuptools
 
 name = 'apache-bookkeeper-client'
 description = 'Apache BookKeeper client library'
-version = '4.15.5'
+version = '4.15.6-alpha-0'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
