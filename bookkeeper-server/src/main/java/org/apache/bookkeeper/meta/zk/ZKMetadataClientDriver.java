7d9e9a6b5009415eabdc2914b3f1f6211357f0a3
congbo <congbo@didiglobal.com>
2024-05-07 20:51:00 +0800

    remove the sync

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/meta/zk/ZKMetadataClientDriver.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/meta/zk/ZKMetadataClientDriver.java
@@ -113,7 +113,7 @@ public class ZKMetadataClientDriver
         });
     }
 
-    public synchronized CompletableFuture<Boolean> isMetadataServiceAvailable() {
+    public CompletableFuture<Boolean> isMetadataServiceAvailable() {
         return CompletableFuture.completedFuture(metadataServiceAvailable);
     }
 }
