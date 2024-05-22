1d82b9278cc8916ca4410bd38f8fea50bfc41aaa
Michael Marshall <mmarshall@apache.org>
2024-05-22 04:23:51 -0500

    Fix ThreadRegistry#register behavior to ensure correct Prom metrics  (#4300)
    * Make tests fail
    
    * Fix ThreadRegistry#register to ensure correct Prom metrics
    
    * Change style to match BK standards
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Nicolò Boschi <boschi1997@gmail.com>

--- stats/bookkeeper-stats-providers/prometheus-metrics-provider/src/main/java/org/apache/bookkeeper/stats/prometheus/ThreadScopedDataSketchesStatsLogger.java
+++ stats/bookkeeper-stats-providers/prometheus-metrics-provider/src/main/java/org/apache/bookkeeper/stats/prometheus/ThreadScopedDataSketchesStatsLogger.java
@@ -22,12 +22,16 @@ import java.util.concurrent.TimeUnit;
 import org.apache.bookkeeper.stats.OpStatsData;
 import org.apache.bookkeeper.stats.OpStatsLogger;
 import org.apache.bookkeeper.stats.ThreadRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * OpStatsLogger implementation that lazily registers OpStatsLoggers per thread
  * with added labels for the threadpool/thresd name and thread no.
  */
 public class ThreadScopedDataSketchesStatsLogger implements OpStatsLogger {
+
+    private static Logger logger = LoggerFactory.getLogger(ThreadScopedDataSketchesStatsLogger.class);
 
     private ThreadLocal<DataSketchesOpStatsLogger> statsLoggers;
     private DataSketchesOpStatsLogger defaultStatsLogger;
@@ -95,8 +99,18 @@ public class ThreadScopedDataSketchesStatsLogger implements OpStatsLogger {
         if (!statsLogger.isThreadInitialized()) {
             ThreadRegistry.ThreadPoolThread tpt = ThreadRegistry.get();
             if (tpt == null) {
+                logger.warn("Thread {} was not registered in the thread registry. Using default stats logger {}.",
+                        Thread.currentThread(), defaultStatsLogger);
                 statsLoggers.set(defaultStatsLogger);
-                provider.opStats.put(new ScopeContext(scopeContext.getScope(), originalLabels), defaultStatsLogger);
+                DataSketchesOpStatsLogger previous = provider.opStats
+                        .put(new ScopeContext(scopeContext.getScope(), originalLabels), defaultStatsLogger);
+                // If we overwrite a logger, metrics will not be collected correctly
+                if (previous != null && previous != defaultStatsLogger) {
+                    logger.error("Invalid state for thead " + Thread.currentThread() + ". Overwrote a stats logger."
+                                    + "New is {}, previous was {}",
+                            defaultStatsLogger, previous);
+                    throw new IllegalStateException("Invalid state. Overwrote a stats logger.");
+                }
                 return defaultStatsLogger;
             } else {
                 Map<String, String> threadScopedlabels = new HashMap<>(originalLabels);
@@ -104,7 +118,15 @@ public class ThreadScopedDataSketchesStatsLogger implements OpStatsLogger {
                 threadScopedlabels.put("thread", String.valueOf(tpt.getOrdinal()));
 
                 statsLogger.initializeThread(threadScopedlabels);
-                provider.opStats.put(new ScopeContext(scopeContext.getScope(), threadScopedlabels), statsLogger);
+                DataSketchesOpStatsLogger previous = provider.opStats
+                        .put(new ScopeContext(scopeContext.getScope(), threadScopedlabels), statsLogger);
+                // If we overwrite a logger, metrics will not be collected correctly
+                if (previous != null && previous != statsLogger) {
+                    logger.error("Invalid state for thead " + Thread.currentThread() + ". Overwrote a stats logger."
+                                    + "New is {}, previous was {}",
+                            defaultStatsLogger, previous);
+                    throw new IllegalStateException("Invalid state. Overwrote a stats logger.");
+                }
             }
         }
 
