70009c19fa3faf306b885794cf1d2fa17ea37e03
wenbingshen <oliver.shen999@gmail.com>
2024-08-16 14:38:18 +0800

    fix checkstyle

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/client/BookKeeperTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/BookKeeperTest.java
@@ -27,7 +27,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
