3353ee71c5829986935b240b8e63b6059fc9494f
StefanoBelli <stefano9913@gmail.com>
2024-09-19 10:03:03 +0200

    Fixing checkstyle issues (3) and new unit test case (BufferedChannelTest)

--- bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/BufferedChannel.java
+++ bookkeeper-server/src/main/java/org/apache/bookkeeper/bookie/BufferedChannel.java
@@ -244,7 +244,7 @@ public class BufferedChannel extends BufferedReadChannel implements Closeable {
     @Override
     public synchronized int read(ByteBuf dest, long pos, int length) throws IOException {
         if (dest.writableBytes() < length) {
-            throw new IllegalArgumentException("dest buffer remaining capacity is not enough" 
+            throw new IllegalArgumentException("dest buffer remaining capacity is not enough"
                     + "(must be at least as \"length\"=" + length + ")");
         }
 
