277a915ecf3e7b4ce2e532a753cee802644fd7af
Zixuan Liu <nodeces@gmail.com>
2026-04-21 12:02:04 +0800

    chore: update groupId from org.apache to com.ascentstream

--- bin/common.sh
+++ bin/common.sh
@@ -116,8 +116,8 @@ CLI_GC_OPTS=${CLI_GC_OPTS:-"${DEFAULT_CLI_GC_OPTS}"}
 CLI_GC_LOGGING_OPTS=${CLI_GC_LOGGING_OPTS:-"${DEFAULT_CLI_GC_LOGGING_OPTS}"}
 
 # module names
-BOOKIE_SERVER_MODULE_NAME="(org.apache.bookkeeper-)?bookkeeper-server"
-TABLE_SERVICE_MODULE_NAME="(org.apache.bookkeeper-)?stream-storage-server"
+BOOKIE_SERVER_MODULE_NAME="(com.ascentstream.bookkeeper-)?bookkeeper-server"
+TABLE_SERVICE_MODULE_NAME="(com.ascentstream.bookkeeper-)?stream-storage-server"
 
 is_released_binary() {
   if [ -d ${BK_HOME}/lib ]; then
