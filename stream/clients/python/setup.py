fb22f25c560b1d10ffd2a43e471f84d1f8cdefb3
Matteo Merli <mmerli@apache.org>
2026-09-11 13:31:04 -0700

    Bump version on branch-4.18 to 4.18.1-SNAPSHOT
    Advance the pom versions to 4.18.1-SNAPSHOT and the Python client to 4.18.1
    in preparation for the 4.18.1 release.

--- stream/clients/python/setup.py
+++ stream/clients/python/setup.py
@@ -19,7 +19,7 @@ import setuptools
 
 name = 'apache_bookkeeper_client'
 description = 'Apache BookKeeper client library'
-version = '4.18.0'
+version = '4.18.1'
 # Should be one of:
 # 'Development Status :: 3 - Alpha'
 # 'Development Status :: 4 - Beta'
